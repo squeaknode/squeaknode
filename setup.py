cea55d63bd4a3c79a5c9fbb468d3cf8af87ad554
Jonathan Zernik <yzernik@gmail.com>
2020-02-22 19:33:40 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import io
+
+from setuptools import find_packages
+from setuptools import setup
+
+with io.open("README.rst", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="squeakserver",
+    version="1.0.0",
+    url="https://flask.palletsprojects.com/tutorial/",
+    license="BSD",
+    maintainer="Pallets team",
+    maintainer_email="contact@palletsprojects.com",
+    description="The basic blog app built in the Flask tutorial.",
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["flask"],
+    extras_require={"test": ["pytest", "coverage"]},
+)
