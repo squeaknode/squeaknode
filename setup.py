5189392f5a246718e60ff5bfd378d6aae77d77c5
Jonathan Zernik <yzernik@gmail.com>
2020-02-28 20:11:12 -0800

    Remove maintainer from setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(
     version="1.0.0",
     url="https://flask.palletsprojects.com/tutorial/",
     license="BSD",
-    maintainer="Pallets team",
-    maintainer_email="contact@palletsprojects.com",
     description="The basic blog app built in the Flask tutorial.",
     long_description=readme,
     packages=find_packages(),
