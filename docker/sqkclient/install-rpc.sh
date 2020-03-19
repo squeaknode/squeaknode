8a63f751750aed7b7527409ddcaabed726e8f48b
Jonathan Zernik <yzernik@gmail.com>
2020-03-19 14:46:25 -0700

    Add script to install rpc

---
+++ docker/sqkclient/install-rpc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install the gRPC files for lnd.
+git clone https://github.com/googleapis/googleapis.git
+curl -o rpc.proto -s https://raw.githubusercontent.com/lightningnetwork/lnd/master/lnrpc/rpc.proto
+
+cp rpc.proto squeaknode/common
+
+python3 -m grpc_tools.protoc --proto_path=googleapis:. --python_out=. --grpc_python_out=. squeaknode/common/rpc.proto
+python3 -m grpc_tools.protoc --proto_path=googleapis:. --python_out=. --grpc_python_out=. squeaknode/client/rpc/route_guide.proto
