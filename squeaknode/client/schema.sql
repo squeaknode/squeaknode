a1435d9009499c7ce11c1cba46ca60a823052c9d
Jonathan Zernik <yzernik@gmail.com>
2020-03-20 17:17:33 -0700

    Save and query squeak in client db.

--- squeaknode/client/schema.sql
+++ squeaknode/client/schema.sql
@@ -3,9 +3,21 @@
 
 DROP TABLE IF EXISTS post;
 
-CREATE TABLE post (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE squeak (
+  hash TEXT PRIMARY KEY,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL
+  nVersion INTEGER NOT NULL,
+  hashEncContent TEXT NOT NULL,
+  hashReplySqk TEXT NOT NULL,
+  hashBlock TEXT NOT NULL,
+  nBlockHeight INTEGER NOT NULL,
+  scriptPubKey BLOB NOT NULL,
+  hashDataKey TEXT NOT NULL,
+  vchIv TEXT NOT NULL,
+  nTime INTEGER NOT NULL,
+  nNonce INTEGER NOT NULL,
+  encContent BLOB NOT NULL,
+  scriptSig BLOB NOT NULL,
+  vchDataKey TEXT NOT NULL,
+  content BLOB NOT NULL
 );
