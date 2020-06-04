4595104bab0cf5434b8bb4abc15c1ef39f9ccd51
yzernik <yzernik@gmail.com>
2020-06-03 23:42:45 -0700

    Use context manager for db connection and cursor

--- squeaknode/server/postgres_db.py
+++ squeaknode/server/postgres_db.py
@@ -8,31 +8,12 @@ class PostgresDb():
 
     def get_connection(self):
         """ Connect to the PostgreSQL database server """
-        conn = None
-        try:
-            # read connection parameters
-            params = self.params
+        with psycopg2.connect(**self.params) as conn:
+            with conn.cursor() as curs:
+	        # execute a statement
+                print('PostgreSQL database version:')
+                curs.execute('SELECT version()')
 
-            # connect to the PostgreSQL server
-            print('Connecting to the PostgreSQL database...')
-            conn = psycopg2.connect(**params)
-
-            # create a cursor
-            cur = conn.cursor()
-
-	    # execute a statement
-            print('PostgreSQL database version:')
-            cur.execute('SELECT version()')
-
-            # display the PostgreSQL database server version
-            db_version = cur.fetchone()
-            print(db_version)
-
-	    # close the communication with the PostgreSQL
-            cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-                print('Database connection closed.')
+                # display the PostgreSQL database server version
+                db_version = curs.fetchone()
+                print(db_version)
