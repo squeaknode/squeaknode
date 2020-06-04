f5c4b5676522672d573f3b73311785c362faefbc
Jonathan Zernik <yzernik@gmail.com>
2020-06-04 11:58:43 -0700

    Clean up client protocol (#21)
    * Remove old method from client protocol
    
    * Remove old route_guide files

--- itests/test.py
+++ itests/test.py
@@ -25,79 +25,6 @@ import route_guide_pb2
 import route_guide_pb2_grpc
 
 
-def make_route_note(message, latitude, longitude):
-    return route_guide_pb2.RouteNote(
-        message=message,
-        location=route_guide_pb2.Point(latitude=latitude, longitude=longitude))
-
-
-def guide_get_one_feature(stub, point):
-    feature = stub.GetFeature(point)
-    if not feature.location:
-        print("Server returned incomplete feature")
-        return
-
-    if feature.name:
-        print("Feature called %s at %s" % (feature.name, feature.location))
-    else:
-        print("Found no feature at %s" % feature.location)
-
-
-def guide_get_feature(stub):
-    guide_get_one_feature(
-        stub,
-        route_guide_pb2.Point(
-            latitude=409146138,
-            longitude=-746188906,
-        ),
-    )
-    guide_get_one_feature(
-        stub,
-        route_guide_pb2.Point(
-            latitude=0,
-            longitude=0,
-        ),
-    )
-
-
-def guide_list_features(stub):
-    rectangle = route_guide_pb2.Rectangle(
-        lo=route_guide_pb2.Point(latitude=400000000, longitude=-750000000),
-        hi=route_guide_pb2.Point(latitude=420000000, longitude=-730000000))
-    print("Looking for features between 40, -75 and 42, -73")
-
-    features = stub.ListFeatures(rectangle)
-
-    for feature in features:
-        print("Feature called %s at %s" % (feature.name, feature.location))
-
-
-def generate_route(feature_list):
-    for _ in range(0, 10):
-        random_feature = feature_list[random.randint(0, len(feature_list) - 1)]
-        print("Visiting point %s" % random_feature.location)
-        yield random_feature.location
-
-
-def generate_messages():
-    messages = [
-        make_route_note("First message", 0, 0),
-        make_route_note("Second message", 0, 1),
-        make_route_note("Third message", 1, 0),
-        make_route_note("Fourth message", 0, 0),
-        make_route_note("Fifth message", 1, 0),
-    ]
-    for msg in messages:
-        print("Sending %s at %s" % (msg.message, msg.location))
-        yield msg
-
-
-def guide_route_chat(stub):
-    responses = stub.RouteChat(generate_messages())
-    for response in responses:
-        print("Received message %s at %s" % (response.message,
-                                             response.location))
-
 def build_squeak_msg(squeak):
     return route_guide_pb2.Squeak(
         hash=squeak.GetHash(),
@@ -125,13 +52,6 @@ def run():
         alice_stub = route_guide_pb2_grpc.RouteGuideStub(alice_channel)
         bob_stub = route_guide_pb2_grpc.RouteGuideStub(bob_channel)
         carol_stub = route_guide_pb2_grpc.RouteGuideStub(carol_channel)
-
-        print("-------------- GetFeature --------------")
-        guide_get_feature(alice_stub)
-        print("-------------- ListFeatures --------------")
-        guide_list_features(alice_stub)
-        print("-------------- RouteChat --------------")
-        guide_route_chat(alice_stub)
 
         print("-------------- WalletBalance --------------")
         balance = alice_stub.WalletBalance(route_guide_pb2.WalletBalanceRequest())
