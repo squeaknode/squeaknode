55d28ca6c94f21b4397df6da2036d588c772f2cd
yzernik <yzernik@gmail.com>
2020-07-18 15:09:56 -0700

    Add btcd credentials and rpc cert to sqkserver configs

---
+++ docker/sqkserver/start-sqkserver.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# exit from script if error was raised.
+set -e
+
+# Add btcd's RPC TLS certificate to system Certificate Authority list.	exec runsqueak \
+cp /rpc/rpc.cert /usr/share/ca-certificates/btcd.crt
+echo btcd.crt >> /etc/ca-certificates.conf
+update-ca-certificates
+
+# To make python requests use the system ca-certificates bundle, it
+# needs to be told to use it over its own embedded bundle
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
+exec runsqueakserver \
+     --config config.ini \
+     --log-level DEBUG \
+     run-server
