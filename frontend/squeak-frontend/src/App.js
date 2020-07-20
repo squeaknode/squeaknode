8560875a71e21ae16f6b9851bfb5449739e1b2a0
yzernik <yzernik@gmail.com>
2020-07-20 01:16:12 -0700

    Got admin rpc request working inside react app

--- frontend/squeak-frontend/src/App.js
+++ frontend/squeak-frontend/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { HelloRequest,
+       HelloReply } from "./proto/squeak_admin_pb"
+import { SqueakAdminClient } from "./proto/squeak_admin_grpc_web_pb"
+
+var client = new SqueakAdminClient('http://' + window.location.hostname + ':8080')
 function App() {
+    console.log('starting App....');
+  const [temp, setTemp] = useState(-9999);
+
+  const getTemp = () => {
+      console.log("called");
+
+      var helloRequest = new HelloRequest()
+      helloRequest.setName('World');
+
+      client.sayHello(helloRequest, {}, (err, response) => {
+	  console.log(response.getMessage());
+      });
+
+      // var stream = client.sayHello(helloRequest,{})
+
+      // stream.on('data', function(response){
+      // 	  console.log('got response: ' + response);
+      //     setTemp(response.getValue())
+      // });
+  };
+
+  useEffect(()=>{
+    getTemp()
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Temperature : {temp} F
     </div>
   );
 }
