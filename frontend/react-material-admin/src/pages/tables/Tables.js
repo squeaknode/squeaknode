4d84dcb90e6abb44cbe844fdf3996016235544fe
Jonathan Zernik <yzernik@gmail.com>
2020-07-20 19:43:15 -0700

    Use react material template for frontend (#98)
    * Use react material design template for frontend
    
    * Successfully make rpc request inside material design template
    
    * Make rpc request in tables page
    
    * Run react frontend in development mode for now
    
    * successfully get squeaks in tables page
    
    * Use production server in frontend dockerfile
    
    * Update gitignore to ignore generated proto files
    
    * Organize npm package

---
+++ frontend/react-material-admin/src/pages/tables/Tables.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import { Grid } from "@material-ui/core";
+import MUIDataTable from "mui-datatables";
+
+// components
+import PageTitle from "../../components/PageTitle";
+import Widget from "../../components/Widget";
+import Table from "../dashboard/components/Table/Table";
+
+// data
+import mock from "../dashboard/mock";
+
+import { HelloRequest,
+       HelloReply,
+       GetFollowedSqueakDisplaysRequest,
+       GetFollowedSqueakDisplaysReply } from "../../proto/squeak_admin_pb"
+import { SqueakAdminClient } from "../../proto/squeak_admin_grpc_web_pb"
+
+var client = new SqueakAdminClient('http://' + window.location.hostname + ':8080')
+
+const datatableData = [
+  ["Joe James", "Example Inc.", "Yonkers", "NY"],
+  ["John Walsh", "Example Inc.", "Hartford", "CT"],
+  ["Bob Herm", "Example Inc.", "Tampa", "FL"],
+  ["James Houston", "Example Inc.", "Dallas", "TX"],
+  ["Prabhakar Linwood", "Example Inc.", "Hartford", "CT"],
+  ["Kaui Ignace", "Example Inc.", "Yonkers", "NY"],
+  ["Esperanza Susanne", "Example Inc.", "Hartford", "CT"],
+  ["Christian Birgitte", "Example Inc.", "Tampa", "FL"],
+  ["Meral Elias", "Example Inc.", "Hartford", "CT"],
+  ["Deep Pau", "Example Inc.", "Yonkers", "NY"],
+  ["Sebastiana Hani", "Example Inc.", "Dallas", "TX"],
+  ["Marciano Oihana", "Example Inc.", "Yonkers", "NY"],
+  ["Brigid Ankur", "Example Inc.", "Dallas", "TX"],
+  ["Anna Siranush", "Example Inc.", "Yonkers", "NY"],
+  ["Avram Sylva", "Example Inc.", "Hartford", "CT"],
+  ["Serafima Babatunde", "Example Inc.", "Tampa", "FL"],
+  ["Gaston Festus", "Example Inc.", "Tampa", "FL"],
+];
+
+export default function Tables() {
+  const [msg, setMsg] = useState("waiting for message...");
+  const [squeaks, setSqueaks] = useState([]);
+  const getMsg = () => {
+      console.log("called getMsg");
+
+      var helloRequest = new HelloRequest()
+      helloRequest.setName('World');
+
+      client.sayHello(helloRequest, {}, (err, response) => {
+	  console.log(response.getMessage());
+	  setMsg(response.getMessage())
+      });
+  };
+  const getSqueaks = () => {
+        console.log("called getSqueaks");
+
+        var getSqueaksRequest = new GetFollowedSqueakDisplaysRequest()
+
+        client.getFollowedSqueakDisplays(getSqueaksRequest, {}, (err, response) => {
+          console.log(response);
+          console.log(response.getSqueakDisplayEntriesList());
+          // console.log(response.getSqueakDisplayEntriesList(),length);
+          setSqueaks(response.getSqueakDisplayEntriesList())
+        });
+  };
+  useEffect(()=>{
+    getMsg()
+  },[]);
+  useEffect(()=>{
+    getSqueaks()
+  },[]);
+
+  return (
+    <>
+      <PageTitle title="Tables" />
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <MUIDataTable
+            title="Employee List"
+            data={datatableData}
+            columns={["Name", "Company", "City", "State"]}
+            options={{
+              filterType: "checkbox",
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Widget title="Material-UI Table" upperTitle noBodyPadding>
+            <Table data={mock.table} />
+          </Widget>
+        </Grid>
+      </Grid>
+
+      <div>
+        Message : {msg}
+      </div>
+
+    </>
+  );
+}
---
+++ frontend/react-material-admin/src/themes/index.js
@@ -0,0 +1,30 @@
+import defaultTheme from "./default";
+
+import { createMuiTheme } from "@material-ui/core";
+
+const overrides = {
+  typography: {
+    h1: {
+      fontSize: "3rem",
+    },
+    h2: {
+      fontSize: "2rem",
+    },
+    h3: {
+      fontSize: "1.64rem",
+    },
+    h4: {
+      fontSize: "1.5rem",
+    },
+    h5: {
+      fontSize: "1.285rem",
+    },
+    h6: {
+      fontSize: "1.142rem",
+    },
+  },
+};
+
+export default {
+  default: createMuiTheme({ ...defaultTheme, ...overrides }),
+};
