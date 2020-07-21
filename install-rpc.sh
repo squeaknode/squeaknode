e8f801d72a7c0cb0c08a5798b136317ff30b20a4
yzernik <yzernik@gmail.com>
2020-07-20 22:51:25 -0700

    Keep local copy of lnd.proto in source code in proto folder

--- install-rpc.sh
+++ install-rpc.sh
@@ -4,13 +4,6 @@ if [ ! -d "googleapis" ]; then
     git clone https://github.com/googleapis/googleapis.git
 fi
 
-if [ ! -f "rpc.proto" ]; then
-    curl -o rpc.proto -s https://raw.githubusercontent.com/lightningnetwork/lnd/master/lnrpc/rpc.proto
-fi
-
-# Move this to squeakserver/common/lnd/rpc.proto instead.
-cp rpc.proto proto/lnd.proto
-
 echo "Installing RPC protocol files"
 python3 -m grpc_tools.protoc --proto_path=googleapis:. --python_out=. --grpc_python_out=. \
 	proto/lnd.proto \
