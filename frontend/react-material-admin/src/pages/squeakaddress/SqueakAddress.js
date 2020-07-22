587e845664c0843f595400614cd2300ab1c1e9ba
yzernik <yzernik@gmail.com>
2020-07-22 01:48:25 -0700

    Load squeak profile for given address in frontend

--- frontend/react-material-admin/src/pages/squeakaddress/SqueakAddress.js
+++ frontend/react-material-admin/src/pages/squeakaddress/SqueakAddress.js
@@ -10,7 +10,9 @@ import PageTitle from "../../components/PageTitle";
 import Widget from "../../components/Widget";
 // import { Typography } from "../../components/Wrappers";
 
-import { GetAddressSqueakDisplaysRequest } from "../../proto/squeak_admin_pb"
+import {
+   GetAddressSqueakDisplaysRequest,
+   GetSqueakProfileByAddressRequest } from "../../proto/squeak_admin_pb"
 import { SqueakAdminClient } from "../../proto/squeak_admin_grpc_web_pb"
 
 var client = new SqueakAdminClient('http://' + window.location.hostname + ':8080')
@@ -19,6 +21,7 @@ export default function SqueakAddressPage() {
   var classes = useStyles();
   const { id } = useParams();
   const [squeaks, setSqueaks] = useState([]);
+  const [squeakProfile, setSqueakProfile] = useState(null);
 
   const getSqueaks = () => {
         console.log("called getSqueaks");
@@ -30,12 +33,27 @@ export default function SqueakAddressPage() {
         client.getAddressSqueakDisplays(getSqueaksRequest, {}, (err, response) => {
           console.log(response);
           console.log(response.getSqueakDisplayEntriesList());
-          // console.log(response.getSqueakDisplayEntriesList(),length);
           setSqueaks(response.getSqueakDisplayEntriesList())
+        });
+  };
+  const getSqueakProfile = () => {
+        console.log("called getSqueakProfile");
+
+        var getSqueakProfileByAddressRequest = new GetSqueakProfileByAddressRequest();
+        getSqueakProfileByAddressRequest.setAddress(id);
+        console.log(getSqueakProfileByAddressRequest);
+
+        client.getSqueakProfileByAddress(getSqueakProfileByAddressRequest, {}, (err, response) => {
+          console.log(response);
+          console.log(response.getSqueakProfile());
+          setSqueakProfile(response.getSqueakProfile());
         });
   };
   useEffect(()=>{
     getSqueaks()
+  },[]);
+  useEffect(()=>{
+    getSqueakProfile()
   },[]);
 
   return (
