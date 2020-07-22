38fa2385c1150ab84a33c6675a470c4760ff6f5d
yzernik <yzernik@gmail.com>
2020-07-22 00:00:56 -0700

    Add profile page

---
+++ frontend/react-material-admin/src/pages/profile/Profile.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Grid } from "@material-ui/core";
+
+// styles
+import useStyles from "./styles";
+
+// components
+import PageTitle from "../../components/PageTitle";
+import Widget from "../../components/Widget";
+// import { Typography } from "../../components/Wrappers";
+
+import { GetAddressSqueakDisplaysRequest } from "../../proto/squeak_admin_pb"
+import { SqueakAdminClient } from "../../proto/squeak_admin_grpc_web_pb"
+
+var client = new SqueakAdminClient('http://' + window.location.hostname + ':8080')
+
+export default function ProfilePage() {
+  var classes = useStyles();
+  const { id } = useParams();
+
+  return (
+    <>
+      <PageTitle title={'Squeak Profile: ' + id} />
+      <Grid container spacing={4}>
+      <div>
+      Hello!
+      </div>
+      </Grid>
+    </>
+  );
+}
