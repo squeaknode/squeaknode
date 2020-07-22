67fd6d6b049cecca08a91130b8a960eaae9a40cc
yzernik <yzernik@gmail.com>
2020-07-22 01:10:45 -0700

    Fetch profile from server in profile page in frontend

--- frontend/react-material-admin/src/pages/profile/Profile.js
+++ frontend/react-material-admin/src/pages/profile/Profile.js
@@ -10,7 +10,7 @@ import PageTitle from "../../components/PageTitle";
 import Widget from "../../components/Widget";
 // import { Typography } from "../../components/Wrappers";
 
-import { GetAddressSqueakDisplaysRequest } from "../../proto/squeak_admin_pb"
+import { GetSqueakProfileRequest } from "../../proto/squeak_admin_pb"
 import { SqueakAdminClient } from "../../proto/squeak_admin_grpc_web_pb"
 
 var client = new SqueakAdminClient('http://' + window.location.hostname + ':8080')
@@ -18,12 +18,44 @@ var client = new SqueakAdminClient('http://' + window.location.hostname + ':8080
 export default function ProfilePage() {
   var classes = useStyles();
   const { id } = useParams();
+  const [squeakProfile, setSqueakProfile] = useState(null);
+
+  const getSqueakProfile = () => {
+        console.log("called getSqueakProfile with profileId: " + id);
+
+        var getSqueaksRequest = new GetSqueakProfileRequest()
+        getSqueaksRequest.setProfileId(id);
+        console.log(getSqueaksRequest);
+
+        client.getSqueakProfile(getSqueaksRequest, {}, (err, response) => {
+          console.log(err);
+          console.log(response);
+          console.log(response.getSqueakProfile());
+          // console.log(response.getSqueakDisplayEntriesList(),length);
+          setSqueakProfile(response.getSqueakProfile())
+        });
+  };
+  useEffect(()=>{
+    getSqueakProfile()
+  },[]);
+
+  function NoProfileContent() {
+    return <p>No profile loaded...</p>;
+  }
+
+  function ProfileContent() {
+    return <p>{squeakProfile.getProfileName()}</p>;
+  }
 
   return (
     <>
-      <PageTitle title={'Squeak Profile: ' + id} />
+      <PageTitle title={'Squeak Profile: ' + (squeakProfile ? squeakProfile.getProfileName() : null)} />
       <div>
       Hello!
+      {squeakProfile
+        ? ProfileContent()
+        : NoProfileContent()
+      }
       </div>
     </>
   );
