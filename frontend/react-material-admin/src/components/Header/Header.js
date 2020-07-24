1ddd70ab927de57f88b71abb2b04eff16b6e3d71
Jonathan Zernik <yzernik@gmail.com>
2020-07-23 17:30:39 -0700

    Remove sample notifications (#133)

--- frontend/react-material-admin/src/components/Header/Header.js
+++ frontend/react-material-admin/src/components/Header/Header.js
@@ -36,27 +36,7 @@ import {
 } from "../../context/LayoutContext";
 import { useUserDispatch, signOut } from "../../context/UserContext";
 
-const notifications = [
-  { id: 0, color: "warning", message: "Check out this awesome ticket" },
-  {
-    id: 1,
-    color: "success",
-    type: "info",
-    message: "What is the best way to get ...",
-  },
-  {
-    id: 2,
-    color: "secondary",
-    type: "notification",
-    message: "This is just a simple notification",
-  },
-  {
-    id: 3,
-    color: "primary",
-    type: "e-commerce",
-    message: "12 new orders has arrived today",
-  },
-];
+const notifications = [];
 
 export default function Header(props) {
   var classes = useStyles();
