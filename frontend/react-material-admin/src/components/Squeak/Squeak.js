31575c207cb28d9cccd8fcd74e3401ef6720791f
yzernik <yzernik@gmail.com>
2020-07-24 00:03:47 -0700

    Fix link to address page from squeak component

--- frontend/react-material-admin/src/components/Squeak/Squeak.js
+++ frontend/react-material-admin/src/components/Squeak/Squeak.js
@@ -46,11 +46,7 @@ export default function Squeak({
           >
             <Grid item>
                 <Box fontWeight="fontWeightBold">
-                  <Link href="#"
-                    onClick={() => {
-                      console.info("I'm a button: " + squeak.getAuthorAddress());
-                      history.push("/app/squeakaddress/" + squeak.getAuthorAddress());
-                    }}
+                  <Link href={"#/app/squeakaddress/" + squeak.getAuthorAddress()}
                     >
                     {squeak.getAuthorName()}
                   </Link>
