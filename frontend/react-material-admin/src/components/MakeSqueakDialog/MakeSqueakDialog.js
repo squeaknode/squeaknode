68bb6d3b43220654d752a81ffabf1dd80e2b9199
yzernik <yzernik@gmail.com>
2020-07-25 21:27:53 -0700

    Add makesqueak dialog component

---
+++ frontend/react-material-admin/src/components/MakeSqueakDialog/MakeSqueakDialog.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+  Paper,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+  Grid,
+  Box,
+  Link,
+} from "@material-ui/core";
+import { MoreVert as MoreIcon } from "@material-ui/icons";
+import {useHistory} from "react-router-dom";
+import classnames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+import Widget from "../../components/Widget";
+
+export default function MakeSqueakDialog({
+  open,
+  handleClose,
+  ...props
+}) {
+  var classes = useStyles();
+
+  // local
+  // var [moreButtonRef, setMoreButtonRef] = useState(null);
+  // var [isMoreMenuOpen, setMoreMenuOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+  <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+  <DialogContent>
+    <DialogContentText>
+      To subscribe to this website, please enter your email address here. We will send updates
+      occasionally.
+    </DialogContentText>
+    <TextField
+      autoFocus
+      margin="dense"
+      id="name"
+      label="Email Address"
+      type="email"
+      fullWidth
+    />
+  </DialogContent>
+  <DialogActions>
+    <Button onClick={handleClose} color="primary">
+      Cancel
+    </Button>
+    <Button onClick={handleClose} color="primary">
+      Subscribe
+    </Button>
+  </DialogActions>
+    </Dialog>
+  )
+}
