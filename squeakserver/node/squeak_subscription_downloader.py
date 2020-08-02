3ee86a06a888cefc8d38654a4fb8cc606ce58a75
yzernik <yzernik@gmail.com>
2020-08-01 22:16:59 -0700

    Run subscriber periodically

--- squeakserver/node/squeak_subscription_downloader.py
+++ squeakserver/node/squeak_subscription_downloader.py
@@ -1,9 +1,22 @@
 import logging
 import queue
+import threading
 
 logger = logging.getLogger(__name__)
 
 
+SUBSCRIBE_UPDATE_INTERVAL_S = 10.0
+
+
 class SqueakSubscriptionDownloader:
-    def __init__(self):
-        pass
+    def __init__(self, postgres_db, squeak_store, update_interval_s=SUBSCRIBE_UPDATE_INTERVAL_S):
+        self.postgres_db = postgres_db
+        self.squeak_store = squeak_store
+        self.update_interval_s = update_interval_s
+
+    def sync_subscriptions(self):
+        logger.info("Syncing subscriptions...")
+
+    def start_running(self):
+        threading.Timer(self.update_interval_s, self.start_running).start()
+        self.sync_subscriptions()
