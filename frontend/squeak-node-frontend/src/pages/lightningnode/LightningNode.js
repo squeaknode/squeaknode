7b28831b798aa6426c8b04f2b1615070cfed3e0b
yzernik <yzernik@gmail.com>
2020-08-17 02:19:20 -0700

    Add tabs to lightning node page

--- frontend/squeak-node-frontend/src/pages/lightningnode/LightningNode.js
+++ frontend/squeak-node-frontend/src/pages/lightningnode/LightningNode.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {useHistory} from "react-router-dom";
 import {
-   Grid,
-   Button,
-   Box,
+  Grid,
+  Button,
+  AppBar,
+  Tabs,
+  Tab,
 } from "@material-ui/core";
 
 // styles
@@ -25,7 +27,18 @@ export default function LightningNodePage() {
   var classes = useStyles();
   const history = useHistory();
   const { pubkey, host } = useParams();
+  const [value, setValue] = useState(0);
 
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   function NoPubkeyContent() {
     return (
@@ -46,11 +59,48 @@ export default function LightningNodePage() {
     )
   }
 
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <div>{children}</div>
+        )}
+      </div>
+    );
+  }
+
+  function LightningNodeTabs() {
+    return (
+      <>
+      <AppBar position="static" color="default">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Balance" {...a11yProps(0)} />
+          <Tab label="Node Info" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        {PubkeyContent()}
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        fooo
+      </TabPanel>
+      </>
+    )
+  }
+
   return (
     <>
       <PageTitle title={'Lightning Node: ' + pubkey} />
       {pubkey
-        ? PubkeyContent()
+        ? LightningNodeTabs()
         : NoPubkeyContent()
       }
     </>
