6a9b347d461f91fc0ba1dd4bd21a2113aa12e35e
yzernik <yzernik@gmail.com>
2020-08-18 23:14:23 -0700

    Define tables in db class

--- squeakserver/server/postgres_db.py
+++ squeakserver/server/postgres_db.py
@@ -8,6 +8,8 @@ from squeak.core import CSqueak
 
 import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, DateTime, Boolean, Binary, BigInteger, MetaData, ForeignKey
+from sqlalchemy import func
 
 from squeakserver.blockchain.util import parse_block_header
 from squeakserver.core.squeak_entry import SqueakEntry
@@ -17,7 +19,7 @@ from squeakserver.core.offer import Offer
 from squeakserver.server.squeak_profile import SqueakProfile
 from squeakserver.server.squeak_peer import SqueakPeer
 from squeakserver.server.util import get_hash
-from squeakserver.db.tables import create_tables
+
 
 logger = logging.getLogger(__name__)
 
@@ -29,6 +31,97 @@ class PostgresDb:
         self.connection_pool = pool.ThreadedConnectionPool(5, 20, **params)
         self.db_string = self.get_connection_string(params)
         self.engine = create_engine(self.db_string)
+
+        self.metadata = MetaData(schema=schema)
+
+        self.users = Table('users', self.metadata,
+                      Column('id', Integer, primary_key=True),
+                      Column('name', String),
+                      Column('fullname', String),
+        )
+
+        self.addresses = Table('addresses', self.metadata,
+                          Column('id', Integer, primary_key=True),
+                          Column('user_id', None, ForeignKey('users.id')),
+                          Column('email_address', String, nullable=False)
+        )
+
+        self.squeaks = Table('squeak', self.metadata,
+                        Column('hash', String(64), primary_key=True),
+                        Column('created', DateTime, server_default=func.now(), nullable=False),
+                        Column('n_version', Integer, nullable=False),
+                        Column('hash_enc_content', String(64), nullable=False),
+                        Column('hash_reply_sqk', String(64), nullable=False),
+                        Column('hash_block', String(64), nullable=False),
+                        Column('n_block_height', Integer, nullable=False),
+                        Column('vch_script_pub_key', Binary, nullable=False),
+                        Column('vch_encryption_key', Binary, nullable=False),
+                        Column('enc_data_key', String, nullable=False),
+                        Column('iv', String(64), nullable=False),
+                        Column('n_time', Integer, nullable=False),
+                        Column('n_nonce', BigInteger, nullable=False),
+                        Column('enc_content', String(2272), nullable=False),
+                        Column('vch_script_sig', Binary, nullable=False),
+                        Column('author_address', String(35), index=True, nullable=False),
+                        Column('vch_decryption_key', Binary, nullable=True),
+                        Column('block_header', Binary, nullable=True),
+        )
+
+        self.profiles = Table('profile', self.metadata,
+                         Column('profile_id', Integer, primary_key=True),
+                         Column('created', DateTime, server_default=func.now(), nullable=False),
+                         Column('profile_name', String, nullable=False),
+                         Column('private_key', Binary),
+                         Column('address', String(35), nullable=False),
+                         Column('sharing', Boolean, nullable=False),
+                         Column('following', Boolean, nullable=False),
+                         Column('whitelisted', Boolean, nullable=False),
+        )
+
+        self.peers = Table('peer', self.metadata,
+                      Column('peer_id', Integer, primary_key=True),
+                      Column('created', DateTime, server_default=func.now(), nullable=False),
+                      Column('peer_name', String),
+                      Column('server_host', String, nullable=False),
+                      Column('server_port', Integer, nullable=False),
+                      Column('uploading', Boolean, nullable=False),
+                      Column('downloading', Boolean, nullable=False),
+        )
+
+        self.offers = Table('offer', self.metadata,
+                       Column('offer_id', Integer, primary_key=True),
+                       Column('created', DateTime, server_default=func.now(), nullable=False),
+                       Column('squeak_hash', String(64), nullable=False),
+                       Column('key_cipher', Binary, nullable=False),
+                       Column('iv', Binary, nullable=False),
+                       Column('payment_hash', String(64), nullable=False),
+                       Column('invoice_timestamp', Integer, nullable=False),
+                       Column('invoice_expiry', Integer, nullable=False),
+                       Column('price_msat', Integer, nullable=False),
+                       Column('payment_request', String, nullable=False),
+                       Column('destination', String(66), nullable=False),
+                       Column('node_host', String, nullable=False),
+                       Column('node_port', Integer, nullable=False),
+                       Column('peer_id', Integer, nullable=False),
+        )
+
+        self.sent_payments = Table('sent_payment', self.metadata,
+                              Column('sent_payment_id', Integer, primary_key=True),
+                              Column('created', DateTime, server_default=func.now(), nullable=False),
+                              Column('offer_id', Integer, nullable=False),
+                              Column('peer_id', Integer, nullable=False),
+                              Column('squeak_hash', String(64), nullable=False),
+                              Column('preimage_hash', String(64), nullable=False),
+                              Column('preimage', String(64), nullable=False),
+                              Column('amount', Integer, nullable=False),
+                              Column('node_pubkey', String(66), nullable=False),
+                              Column('preimage_is_valid', Boolean, nullable=False),
+        )
+
+    def create_tables(self):
+        logger.info("Calling create_tables")
+        self.metadata.create_all(self.engine)
+        logger.info("Called create_tables")
 
     # Get Cursor
     @contextmanager
@@ -65,7 +158,8 @@ class PostgresDb:
         logger.info("SqlAlchemy version: {}".format(sqlalchemy.__version__))
 
         logger.info("Creating tables...")
-        create_tables(self.engine, self.schema)
+        # create_tables(self.engine, self.schema)
+        self.create_tables()
         logger.info("Created tables.")
 
         # with self.get_cursor() as curs:
