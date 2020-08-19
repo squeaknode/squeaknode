589303edca934f35b90b902835cfdaa64af55336
yzernik <yzernik@gmail.com>
2020-08-18 21:59:50 -0700

    Use sqlalchemy to create squeak table

--- init.sql
+++ init.sql
@@ -1,26 +1,26 @@
-CREATE TABLE IF NOT EXISTS squeak (
-  hash CHAR(64) PRIMARY KEY,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  n_version INTEGER NOT NULL,
-  hash_enc_content CHAR(64) NOT NULL,
-  hash_reply_sqk CHAR(64) NOT NULL,
-  hash_block CHAR(64) NOT NULL,
-  n_block_height INTEGER NOT NULL,
-  vch_script_pub_key bytea NOT NULL,
-  vch_encryption_key bytea NOT NULL,
-  enc_data_key CHAR(256) NOT NULL,
-  iv CHAR(32) NOT NULL,
-  n_time INTEGER NOT NULL,
-  n_nonce BIGINT NOT NULL,
-  enc_content CHAR(2272) NOT NULL, -- Encrypted content length is always 1136 bytes (2272 hex characters).
-  vch_script_sig bytea NOT NULL,
-  author_address VARCHAR(35) NOT NULL, -- Maximum length of a bitcoin address is 35.
-  vch_decryption_key bytea,
-  block_header bytea
-);
+-- CREATE TABLE IF NOT EXISTS squeak (
+--   hash CHAR(64) PRIMARY KEY,
+--   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   n_version INTEGER NOT NULL,
+--   hash_enc_content CHAR(64) NOT NULL,
+--   hash_reply_sqk CHAR(64) NOT NULL,
+--   hash_block CHAR(64) NOT NULL,
+--   n_block_height INTEGER NOT NULL,
+--   vch_script_pub_key bytea NOT NULL,
+--   vch_encryption_key bytea NOT NULL,
+--   enc_data_key CHAR(256) NOT NULL,
+--   iv CHAR(32) NOT NULL,
+--   n_time INTEGER NOT NULL,
+--   n_nonce BIGINT NOT NULL,
+--   enc_content CHAR(2272) NOT NULL, -- Encrypted content length is always 1136 bytes (2272 hex characters).
+--   vch_script_sig bytea NOT NULL,
+--   author_address VARCHAR(35) NOT NULL, -- Maximum length of a bitcoin address is 35.
+--   vch_decryption_key bytea,
+--   block_header bytea
+-- );
 
-CREATE INDEX IF NOT EXISTS idx_squeak_address
-  ON squeak(author_address);
+-- CREATE INDEX IF NOT EXISTS idx_squeak_address
+--   ON squeak(author_address);
 
 CREATE TABLE IF NOT EXISTS profile (
   profile_id SERIAL PRIMARY KEY,
--- squeakserver/db/tables.py
+++ squeakserver/db/tables.py
@@ -1,6 +1,6 @@
 import logging
 
-from sqlalchemy import Table, Column, Integer, String, DateTime, Boolean, MetaData, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, DateTime, Boolean, Binary, BigInteger, MetaData, ForeignKey
 from sqlalchemy import func
 
 
@@ -22,6 +22,27 @@ def create_tables(engine, schema):
                       Column('id', Integer, primary_key=True),
                       Column('user_id', None, ForeignKey('users.id')),
                       Column('email_address', String, nullable=False)
+    )
+
+    squeaks = Table('squeak', metadata,
+                  Column('hash', String(64), primary_key=True),
+                  Column('created', DateTime, server_default=func.now(), nullable=False),
+                  Column('n_version', Integer, nullable=False),
+                  Column('hash_enc_content', String(64), nullable=False),
+                  Column('hash_reply_sqk', String(64), nullable=False),
+                  Column('hash_block', String(64), nullable=False),
+                  Column('n_block_height', Integer, nullable=False),
+                  Column('vch_script_pub_key', Binary, nullable=False),
+                  Column('vch_encryption_key', Binary, nullable=False),
+                  Column('enc_data_key', String, nullable=False),
+                  Column('iv', String(64), nullable=False),
+                  Column('n_time', Integer, nullable=False),
+                  Column('n_nonce', BigInteger, nullable=False),
+                  Column('enc_content', String(2272), nullable=False),
+                  Column('vch_script_sig', Binary, nullable=False),
+                  Column('author_address', String(35), nullable=False),
+                  Column('vch_decryption_key', Binary, nullable=True),
+                  Column('block_header', Binary, nullable=True),
     )
 
     peers = Table('peer', metadata,
