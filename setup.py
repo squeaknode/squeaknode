6eb9cbf5cc87c7168137dee498ec31f573bedbe3
yzernik <yzernik@gmail.com>
2020-08-20 04:25:05 -0700

    Generate python proto files in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ import io
 
 from setuptools import find_packages, setup
 
+from grpc_tools.command import BuildPackageProtos
+
+
+PACKAGE_DIRECTORIES = {
+    '': '.',
+}
+
 with io.open("README.md", "rt", encoding="utf8") as f:
     readme = f.read()
 
@@ -12,6 +19,7 @@ setup(
     description="Server for squeak protocol.",
     long_description=readme,
     packages=find_packages(),
+    package_dir=PACKAGE_DIRECTORIES,
     include_package_data=True,
     zip_safe=False,
     extras_require={"test": ["pytest", "coverage"]},
@@ -19,5 +27,8 @@ setup(
         'console_scripts': [
             'runsqueakserver = squeakserver.main:main',
         ],
+    },
+    cmdclass={
+        'build_proto_modules': BuildPackageProtos,
     },
 )
