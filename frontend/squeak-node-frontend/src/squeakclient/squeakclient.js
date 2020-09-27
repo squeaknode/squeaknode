4b9e2dc8de252078179743ce97c12384898c8f6b
yzernik <yzernik@gmail.com>
2020-09-27 04:48:10 -0700

    Use second instance of squeak node in docker compose

--- frontend/squeak-node-frontend/src/squeakclient/squeakclient.js
+++ frontend/squeak-node-frontend/src/squeakclient/squeakclient.js
@@ -1,3 +1,10 @@
 import { SqueakAdminClient } from "../proto/squeak_admin_grpc_web_pb"
 
-export let client = new SqueakAdminClient('http://' + window.location.hostname + ':8080')
+console.log("fooooooo")
+
+const PORT = process.env.REACT_APP_PORT;
+
+console.log("port:");
+console.log(PORT);
+
+export let client = new SqueakAdminClient('http://' + window.location.hostname + ':' + PORT)
