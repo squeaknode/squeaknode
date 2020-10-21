7beab4884e529302bd23fc8ff7b6e3573b0f29c2
yzernik <yzernik@gmail.com>
2020-10-20 22:35:50 -0400

    Got web frontend server running inside sqkserver

--- squeakserver/admin/squeak_admin_web_service.py
+++ squeakserver/admin/squeak_admin_web_service.py
@@ -1,14 +1,27 @@
+import os
+import logging
+
 from flask import Flask
-app = Flask(__name__)
+
+
+logger = logging.getLogger(__name__)
+
+
+logger.info("Starting flask app from directory: {}".format(os.getcwd()))
+app = Flask(__name__, static_folder='/app/static/build', static_url_path='/')
 
 
 @app.route('/')
 def index():
+    logger.info("Getting index route.")
+    logger.info("os.getcwd(): {}".format(os.getcwd()))
+    logger.info("os.listdir(os.getcwd()): {}".format(os.listdir(os.getcwd())))
     return app.send_static_file('index.html')
 
 
 @app.route('/hello')
 def hello_world():
+    logger.info("Getting hello route.")
     return 'Hello, World!'
 
 
