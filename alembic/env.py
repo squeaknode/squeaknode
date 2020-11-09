f99928f3d23e90632dfb952b2acb3aea471d8951
yzernik <yzernik@gmail.com>
2020-11-08 19:00:47 -0500

    Add target metadata to alembic config

--- alembic/env.py
+++ alembic/env.py
@@ -17,7 +17,8 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from squeaknode.db.models import Models
+target_metadata = Models().metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
