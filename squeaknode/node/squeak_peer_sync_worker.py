10cec10de8c9b308ea72f424fdf2fcf2fbf8b366
yzernik <yzernik@gmail.com>
2020-11-09 22:36:22 -0500

    Add sync_interval_s config

--- squeaknode/node/squeak_peer_sync_worker.py
+++ squeaknode/node/squeak_peer_sync_worker.py
@@ -4,22 +4,20 @@ import threading
 logger = logging.getLogger(__name__)
 
 
-SUBSCRIBE_UPDATE_INTERVAL_S = 10.0
-
-
 class SqueakPeerSyncWorker:
     def __init__(
         self,
         squeak_sync_controller,
-        update_interval_s=SUBSCRIBE_UPDATE_INTERVAL_S,
+        sync_interval_s,
     ):
         self.squeak_sync_controller = squeak_sync_controller
-        self.update_interval_s = update_interval_s
+        self.sync_interval_s = sync_interval_s
 
     def sync_timeline(self):
         logger.info("Syncing timeline with peers...")
         self.squeak_sync_controller.sync_timeline()
 
     def start_running(self):
-        threading.Timer(self.update_interval_s, self.start_running).start()
-        self.sync_timeline()
+        if self.sync_interval_s:
+            threading.Timer(self.sync_interval_s, self.start_running).start()
+            self.sync_timeline()
