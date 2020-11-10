efd12e8332274070d7dbaaa467cc930b56020d4b
Jonathan Zernik <yzernik@gmail.com>
2020-11-09 21:43:51 -0500

    Fix alembic messing up logging (#423)

--- squeaknode/db/migrations.py
+++ squeaknode/db/migrations.py
@@ -5,6 +5,7 @@ from alembic import command
 def run_migrations(engine):
     """ Run migrations. """
     alembic_cfg = Config("alembic.ini")
+    alembic_cfg.attributes['configure_logger'] = False
     with engine.begin() as connection:
         alembic_cfg.attributes['connection'] = connection
         command.upgrade(alembic_cfg, "head")
