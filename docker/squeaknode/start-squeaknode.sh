54697354c55419bfb12f71b0b8e3c8ee820fa69a
yzernik <yzernik@gmail.com>
2020-11-24 00:03:29 -0500

    Remove shared volume for btcd cert file

--- docker/squeaknode/start-squeaknode.sh
+++ docker/squeaknode/start-squeaknode.sh
@@ -3,20 +3,20 @@
 # exit from script if error was raised.
 set -e
 
-# Add btcd's RPC TLS certificate to system Certificate Authority list.	exec runsqueak \
-cp /rpc/rpc.cert /usr/share/ca-certificates/btcd.crt
-echo btcd.crt >> /etc/ca-certificates.conf
-update-ca-certificates
+# # Add btcd's RPC TLS certificate to system Certificate Authority list.	exec runsqueak \
+# cp /rpc/rpc.cert /usr/share/ca-certificates/btcd.crt
+# echo btcd.crt >> /etc/ca-certificates.conf
+# update-ca-certificates
 
-# To make python requests use the system ca-certificates bundle, it
-# needs to be told to use it over its own embedded bundle
-export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+# # To make python requests use the system ca-certificates bundle, it
+# # needs to be told to use it over its own embedded bundle
+# export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 
-# Wait for the lnd cert file to exist before starting
-while ! test -f "/root/.lnd/tls.cert"; do
-    sleep 10
-    echo "Still waiting for lnd cert file to exist..."
-done
+# # Wait for the lnd cert file to exist before starting
+# while ! test -f "/root/.lnd/tls.cert"; do
+#     sleep 10
+#     echo "Still waiting for lnd cert file to exist..."
+# done
 
 # Start using the run server command
 exec runsqueaknode \
