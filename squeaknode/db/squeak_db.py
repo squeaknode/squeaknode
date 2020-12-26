eaf26bd5569b76bad7377e69f072a87bb1b0e9f4
Jonathan Zernik <yzernik@gmail.com>
2020-12-25 20:23:20 -0800

    Remove old migrations comment (#504)

--- squeaknode/db/squeak_db.py
+++ squeaknode/db/squeak_db.py
@@ -38,14 +38,6 @@ from squeaknode.core.received_payment import ReceivedPayment
 logger = logging.getLogger(__name__)
 
 
-# def run_migrations(script_location: str, dsn: str) -> None:
-#     LOG.info('Running DB migrations in %r on %r', script_location, dsn)
-#     alembic_cfg = Config()
-#     #alembic_cfg.set_main_option('script_location', script_location)
-#     alembic_cfg.set_main_option('sqlalchemy.url', dsn)
-#     command.upgrade(alembic_cfg, 'head')
-
-
 class SqueakDb:
     def __init__(self, engine, schema=None):
         self.engine = engine
