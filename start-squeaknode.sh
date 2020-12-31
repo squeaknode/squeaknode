af6910dfcfe4a904f9965073a3b1c479d62138a0
yzernik <yzernik@gmail.com>
2020-12-30 21:23:13 -0800

    Remove waiting for cert file in start script

--- start-squeaknode.sh
+++ start-squeaknode.sh
@@ -3,11 +3,11 @@
 # exit from script if error was raised.
 set -e
 
-# Wait for the lnd cert file to exist before starting
-while ! test -f "/root/.lnd/tls.cert"; do
-    sleep 10
-    echo "Still waiting for lnd cert file to exist..."
-done
+# # Wait for the lnd cert file to exist before starting
+# while ! test -f "/root/.lnd/tls.cert"; do
+#     sleep 10
+#     echo "Still waiting for lnd cert file to exist..."
+# done
 
 # Start using the run server command
 exec runsqueaknode \
