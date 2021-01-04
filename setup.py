5f371d319c08811b23b0825eeb2149ba3374c7de
yzernik <yzernik@gmail.com>
2021-01-03 17:03:27 -0800

    Change grpcio version

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
         'importlib_resources',
         'argparse',
         'googleapis-common-protos',
-        'grpcio>=1.27.0',
+        'grpcio>=1.21.0',
         'grpcio-tools',
         'mypy-protobuf',
         'psycopg2',
