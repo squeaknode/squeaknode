216588afbd6f9cebba0009a4b13f29a364c969d2
yzernik <yzernik@gmail.com>
2021-01-11 00:10:57 -0800

    Remove unnecessary fields from BuyOffer

--- squeaknode/sync/util.py
+++ squeaknode/sync/util.py
@@ -10,10 +10,8 @@ logger = logging.getLogger(__name__)
 def parse_buy_offer(buy_offer_msg: squeak_server_pb2.SqueakBuyOffer) -> BuyOffer:
     return BuyOffer(
         squeak_hash=buy_offer_msg.squeak_hash,
-        price_msat=0,
         nonce=buy_offer_msg.nonce,
         payment_request=buy_offer_msg.payment_request,
-        pubkey="",
         host=buy_offer_msg.host,
         port=buy_offer_msg.port,
     )
