1969308b20a462cc04b70619473930ad3f593f18
Jonathan Zernik <yzernik@gmail.com>
2021-01-11 11:26:59 -0800

    Delete unused modules (#600)

--- squeaknode/node/squeak_expired_offer_cleaner.py
+++
@@ -1,7 +0,0 @@
-# import logging
-# logger = logging.getLogger(__name__)
-# class SqueakExpiredOfferCleaner:
-#     def __init__(self, squeak_controller):
-#         self.squeak_controller = squeak_controller
-#     def delete_all_expired_offers(self):
-#         self.squeak_controller.delete_all_expired_offers()
