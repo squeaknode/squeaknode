63fad03146686070e78f2141dc698531e8f2a3b5
yzernik <yzernik@gmail.com>
2021-01-12 20:33:29 -0800

    Remove unused lookup response class

--- squeaknode/sync/lookup_response.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from typing import NamedTuple
-
-
-class LookupResponse(NamedTuple):
-    """Represents a response to a lookup."""
-    hashes: List[bytes]
-    allowed_addresses: List[str]
