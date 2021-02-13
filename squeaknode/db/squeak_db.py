ef2da125802998b2dcb85f3cdb988c65e8d77c53
yzernik <yzernik@gmail.com>
2021-02-13 03:38:11 -0800

    Add TODO comment for insert squeak method

--- squeaknode/db/squeak_db.py
+++ squeaknode/db/squeak_db.py
@@ -122,7 +122,8 @@ class SqueakDb:
 
     def insert_squeak(self, squeak: CSqueak, block_header: CBlockHeader) -> bytes:
         """ Insert a new squeak.
-        TODO: This method should not modify the squeak object (decryption key).
+        TODO: Clear the decryption key from the serialized bytes
+        without modifying the passed squeak object.
 
         Return the hash (bytes) of the inserted squeak.
         """
