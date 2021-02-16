a74e81a777a716cb9113cefa83b5f6024da2f367
yzernik <yzernik@gmail.com>
2021-02-16 05:33:53 -0800

    Revert "Trying to get payment processor working without passing stopped to subscripion"
    This reverts commit e691a37d2459b61bf031022479d3f69b9a51d602.

--- squeaknode/node/payment_processor.py
+++ squeaknode/node/payment_processor.py
@@ -21,57 +21,39 @@ class PaymentProcessor:
         self.retry_s = retry_s
         self.stopped = threading.Event()
         self.lock = threading.Lock()
-        self._cancel_fn = None
 
     def start_processing(self):
         with self.lock:
             self.stopped.set()
-            self._cancel_subscription()
             self.stopped.clear()
             threading.Thread(
-                target=self._process_subscribed_invoices,
+                target=self.process_subscribed_invoices,
             ).start()
 
     def stop_processing(self):
         with self.lock:
             self.stopped.set()
-            self._cancel_subscription()
 
-    def _cancel_subscription(self):
-        logger.info("Cancelling subscription...")
-        if self._cancel_fn is not None:
-            self._cancel_fn()
-            logger.info("Cancelled subscription.")
+    def process_subscribed_invoices(self):
+        def get_sent_offer_for_payment_hash(payment_hash: bytes) -> SentOffer:
+            return self.squeak_db.get_sent_offer_by_payment_hash(
+                payment_hash
+            )
 
-    def _process_subscribed_invoices(self):
-        received_payments_result = self.squeak_core.get_received_payments(
-            self.get_latest_settle_index(),
-            self.get_sent_offer_for_payment_hash,
-        )
-        self._cancel_fn = received_payments_result.cancel_fn
-        for received_payment in received_payments_result.result_stream:
+        def get_latest_settle_index():
+            return self.squeak_db.get_latest_settle_index() or 0
+        for received_payment in self.squeak_core.get_received_payments(
+                get_sent_offer_for_payment_hash,
+                get_latest_settle_index,
+                self.stopped,
+                retry_s=self.retry_s,
+        ):
             logger.info(
                 "Got received payment: {}".format(received_payment))
             try:
-                self.squeak_db.insert_received_payment(received_payment)
+                self.squeak_db.insert_received_payment(
+                    received_payment)
             except DuplicateReceivedPaymentError:
                 pass
-            # self.squeak_db.delete_sent_offer(received_payment.payment_hash)
-
-    def get_sent_offer_for_payment_hash(self, payment_hash: bytes) -> SentOffer:
-        logger.info("Current sent offers: {}".format(
-            self.squeak_db.get_sent_offers(),
-        ))
-        logger.info("Getting sent offer for payment hash: {}".format(
-            payment_hash.hex(),
-        ))
-        sent_offer = self.squeak_db.get_sent_offer_by_payment_hash(
-            payment_hash
-        )
-        logger.info("Got sent offer: {}".format(
-            sent_offer,
-        ))
-        return sent_offer
-
-    def get_latest_settle_index(self) -> int:
-        return self.squeak_db.get_latest_settle_index() or 0
+            self.squeak_db.delete_sent_offer(
+                received_payment.payment_hash)
