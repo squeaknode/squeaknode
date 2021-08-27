1aa3af8b9a4320e43c057042e6f01fc8c6b06545
Jonathan Zernik <yzernik@gmail.com>
2021-08-27 05:40:31 -0700

    Use icon for paginate button (#1074)
    * Add icon to paginate button on timeline page
    
    * Update frontend build

---
+++ squeaknode/admin/webapp/static/build/precache-manifest.ee0732b6174e80bd6e6324ed8fc3545d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5000d404a4fcd12b585855011b5f1aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23bb3a596c7a4e008d8",
+    "url": "/static/css/2.ea4ba2f0.chunk.css"
+  },
+  {
+    "revision": "f23bb3a596c7a4e008d8",
+    "url": "/static/js/2.b9c300d2.chunk.js"
+  },
+  {
+    "revision": "7156b2c9571000777b9be03b3c6006ee",
+    "url": "/static/js/2.b9c300d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4559792f9d9c64006e72",
+    "url": "/static/js/main.d4b4f12c.chunk.js"
+  },
+  {
+    "revision": "cc9816de5a8639d377ea",
+    "url": "/static/js/runtime-main.9f0ba400.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/static/media/logo.a0185b04.svg"
+  }
+]);
