576e0294bf4a127112b81e5c3e9dc86cea3d9d40
yzernik <yzernik@gmail.com>
2021-08-27 21:17:23 -0700

    Remove old comments from connection manager

--- squeaknode/network/connection_manager.py
+++ squeaknode/network/connection_manager.py
@@ -45,7 +45,6 @@ class ConnectionManager(object):
     def __init__(self):
         self._peers: Dict[PeerAddress, Peer] = {}
         self.peers_lock = threading.Lock()
-        # self.peers_changed_callbacks = {}
         self.peer_changed_listener = EventListener()
 
     @property
@@ -63,12 +62,7 @@ class ConnectionManager(object):
         for peer in peers:
             logger.info(peer)
         logger.info('--------------')
-        # for callback in self.peers_changed_callbacks.values():
-        #     callback(peers)
         self.peer_changed_listener.handle_new_item(peers)
-
-    # def listen_peers_changed(self, callback):
-    #     self.peers_changed_callback = callback
 
     def _is_duplicate_nonce(self, peer):
         for other_peer in self.peers:
@@ -121,12 +115,6 @@ class ConnectionManager(object):
         with self.peers_lock:
             for peer in self.peers:
                 peer.stop()
-
-    # def add_peers_changed_callback(self, name, callback):
-    #     self.peers_changed_callbacks[name] = callback
-
-    # def remove_peers_changed_callback(self, name):
-    #     del self.peers_changed_callbacks[name]
 
     def yield_peers_changed(self, stopped: threading.Event):
         for item in self.peer_changed_listener.yield_items(stopped):
