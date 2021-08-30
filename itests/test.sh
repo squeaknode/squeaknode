7ed21a672fb573443a1fb4c1e346a58666c39c63
yzernik <yzernik@gmail.com>
2021-08-30 05:11:19 -0700

    Fix loading buy offers from stream

--- itests/test.sh
+++ itests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#pytest -s tests
+pytest -s tests
 #pytest -s tests -k "test_buy_squeak"
 #pytest -s tests -k "test_sell_squeak"
 #pytest -s tests -k "test_download_single_squeak"
@@ -10,4 +10,4 @@
 #pytest -s tests -k "test_share_single_squeak"
 #pytest -s tests -k "test_delete_squeak"
 #pytest -s tests -k "test_subscribe_squeaks"
-pytest -s tests -k "test_download_squeaks_for_address"
+#pytest -s tests -k "test_download_squeaks_for_address"
