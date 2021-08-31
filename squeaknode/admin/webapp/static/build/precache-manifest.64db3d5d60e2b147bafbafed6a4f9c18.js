deb1c9282b911f79a56a41e0288dfd2c6a6b5217
yzernik <yzernik@gmail.com>
2021-08-31 01:23:58 -0700

    Update frontend build

---
+++ squeaknode/admin/webapp/static/build/precache-manifest.64db3d5d60e2b147bafbafed6a4f9c18.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05be7bd1ef4cffb3b9b9f29e7ce71134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87783a5fbc1ae331e31c",
+    "url": "/static/css/2.ea4ba2f0.chunk.css"
+  },
+  {
+    "revision": "87783a5fbc1ae331e31c",
+    "url": "/static/js/2.74e88d43.chunk.js"
+  },
+  {
+    "revision": "279b8724b89bf7d504758b3fa917239f",
+    "url": "/static/js/2.74e88d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd9ea1f28e7d38cc64f",
+    "url": "/static/js/main.45beaf28.chunk.js"
+  },
+  {
+    "revision": "cc9816de5a8639d377ea",
+    "url": "/static/js/runtime-main.9f0ba400.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/static/media/logo.a0185b04.svg"
+  }
+]);
