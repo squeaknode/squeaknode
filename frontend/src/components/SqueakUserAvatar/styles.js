eef2a2b1fae8c09dfe8c43d1672448d2ae4a1a70
yzernik <yzernik@gmail.com>
2021-09-01 10:12:56 -0700

    Make avatar even bigger

--- frontend/src/components/SqueakUserAvatar/styles.js
+++ frontend/src/components/SqueakUserAvatar/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/styles';
 
 export default makeStyles((theme) => ({
   large: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
+    width: theme.spacing(8),
+    height: theme.spacing(8),
   },
 }));
