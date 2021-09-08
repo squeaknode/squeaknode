bb25bfbd0af07cae83314aa1531dd479e08546c7
yzernik <yzernik@gmail.com>
2021-09-08 10:41:13 -0700

    Update frontend build

--- squeaknode/admin/webapp/static/build/service-worker.js
+++ squeaknode/admin/webapp/static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5f2492faa6e868a5f1b2d46bb8144db.js"
+  "/precache-manifest.7eef597035e3601a05613393510db816.js"
 );
 
 self.addEventListener('message', (event) => {
