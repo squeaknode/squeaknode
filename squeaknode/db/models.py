6393489fc1d377d587995fc8672fd6f63281ea56
Jonathan Zernik <yzernik@gmail.com>
2021-09-09 23:32:25 -0700

    Delete migrations and start fresh (#1264)
    * Set up initialize all migration
    
    * Delete tzdatetime class

--- squeaknode/db/models.py
+++ squeaknode/db/models.py
@@ -19,42 +19,18 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import datetime
 import logging
 
 from sqlalchemy import BigInteger
 from sqlalchemy import Binary
 from sqlalchemy import Boolean
 from sqlalchemy import Column
-from sqlalchemy import DateTime
 from sqlalchemy import Integer
 from sqlalchemy import MetaData
 from sqlalchemy import String
 from sqlalchemy import Table
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.types import TypeDecorator
-
-
-logger = logging.getLogger(__name__)
-
-
-class TZDateTime(TypeDecorator):
-    impl = DateTime
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            if not value.tzinfo:
-                raise TypeError("tzinfo is required")
-            value = value.astimezone(datetime.timezone.utc).replace(
-                tzinfo=None
-            )
-        return value
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            value = value.replace(tzinfo=datetime.timezone.utc)
-        return value
 
 
 logger = logging.getLogger(__name__)
