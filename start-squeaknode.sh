5a79792def83f2f5e4bee33fbf735019dc8b0a79
yzernik <yzernik@gmail.com>
2021-09-15 19:01:44 -0700

    Remove sleep from start script

--- start-squeaknode.sh
+++ start-squeaknode.sh
@@ -3,14 +3,6 @@
 # exit from script if error was raised.
 set -e
 
-# # Wait for the lnd cert file to exist before starting
-# while ! test -f "/root/.lnd/tls.cert"; do
-#     sleep 10
-#     echo "Still waiting for lnd cert file to exist..."
-# done
-
-sleep 10
-
 # Start using the run server command
 if [ -f config.ini ]; then
     exec squeaknode \
