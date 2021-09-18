544887d0df88e5e7de84b838a02d51b87c05bf9a
yzernik <yzernik@gmail.com>
2021-09-18 14:49:42 -0700

    Show waiting indicator for make squeak dialog click

--- frontend/src/components/MakeSqueakDialog/styles.js
+++ frontend/src/components/MakeSqueakDialog/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/styles';
+import { green } from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
   widgetWrapper: {
@@ -39,5 +40,30 @@ export default makeStyles((theme) => ({
       backgroundColor: theme.palette.primary.main,
       color: 'rgba(255, 255, 255, 0.35)',
     },
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+  fabProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: -6,
+    left: -6,
+    zIndex: 1,
+  },
+  buttonProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
   },
 }));
