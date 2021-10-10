62cef77879710929793916c24ed10f018159e5bb
yzernik <yzernik@gmail.com>
2021-10-10 00:29:55 -0700

    Reset port back to empty when use custom is checked or unchecked.

--- frontend/src/components/ConnectPeerDialog/ConnectPeerDialog.js
+++ frontend/src/components/ConnectPeerDialog/ConnectPeerDialog.js
@@ -61,6 +61,7 @@ export default function ConnectPeerDialog({
     // setPort(
     //   event.target.checked ? '' : defaultPeerPort,
     // );
+    setPort('');
     setCustomPortChecked(event.target.checked);
   };
 
--- frontend/src/components/CreatePeerDialog/CreatePeerDialog.js
+++ frontend/src/components/CreatePeerDialog/CreatePeerDialog.js
@@ -96,6 +96,7 @@ export default function CreatePeerDialog({
     // setPort(
     //   event.target.checked ? '' : portDefaultValue,
     // );
+    setPort('');
     setCustomPortChecked(event.target.checked);
   };
 
