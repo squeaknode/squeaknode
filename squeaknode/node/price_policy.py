a89339c21c7373604217d1508c42fee49acb16d5
yzernik <yzernik@gmail.com>
2021-11-07 04:23:11 -0800

    Add price policy class to determine sell price

---
+++ squeaknode/node/price_policy.py
@@ -0,0 +1,65 @@
+# MIT License
+#
+# Copyright (c) 2020 Jonathan Zernik
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import logging
+from typing import Optional
+
+from squeak.core import CSqueak
+
+from squeaknode.config.config import SqueaknodeConfig
+from squeaknode.core.peer_address import PeerAddress
+from squeaknode.core.squeak_peer import SqueakPeer
+from squeaknode.core.squeak_profile import SqueakProfile
+from squeaknode.db.squeak_db import SqueakDb
+
+
+logger = logging.getLogger(__name__)
+
+
+class PricePolicy:
+
+    def __init__(self, squeak_db: SqueakDb, config: SqueaknodeConfig):
+        self.squeak_db = squeak_db
+        self.config = config
+
+    def get_price(self, squeak: CSqueak, peer_address: PeerAddress) -> int:
+        """Get the price to sell this squeak to this peer.
+
+        """
+        # Return zero for price if peer is configured to be share for free.
+        peer = self.get_peer(peer_address)
+        if peer is not None and peer.share_for_free:
+            return 0
+        # Return custom price if address is configured with custom price.
+        squeak_address = str(squeak.GetAddress())
+        squeak_profile = self.get_profile(squeak_address)
+        if squeak_profile is not None and squeak_profile.use_custom_price:
+            return squeak_profile.custom_price_msat
+        return self.get_default_price()
+
+    def get_peer(self, peer_address: PeerAddress) -> Optional[SqueakPeer]:
+        return self.squeak_db.get_peer_by_address(peer_address)
+
+    def get_profile(self, address: str) -> Optional[SqueakProfile]:
+        return self.squeak_db.get_profile_by_address(address)
+
+    def get_default_price(self) -> int:
+        return self.config.node.price_msat
