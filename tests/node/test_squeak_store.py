79f0d854ca62b183b08796cdc36a91c771f55d93
yzernik <yzernik@gmail.com>
2021-12-27 17:36:22 -0800

    Use mock db for tests in squeak store

--- tests/node/test_squeak_store.py
+++ tests/node/test_squeak_store.py
@@ -21,7 +21,6 @@
 # SOFTWARE.
 import mock
 import pytest
-from sqlalchemy import create_engine
 
 from squeaknode.core.lightning_address import LightningAddressHostPort
 from squeaknode.core.squeak_core import SqueakCore
@@ -30,16 +29,8 @@ from squeaknode.node.squeak_store import SqueakStore
 
 
 @pytest.fixture
-def db_engine():
-    yield create_engine('sqlite://')
-
-
-@pytest.fixture
-def squeak_db(db_engine):
-    # TODO: use a mock object for db.
-    db = SqueakDb(db_engine)
-    db.init()
-    yield db
+def squeak_db():
+    return mock.Mock(spec=SqueakDb)
 
 
 @pytest.fixture
@@ -109,8 +100,12 @@ def squeak_store(
 
 
 def test_save_squeak(squeak_store, squeak_db, squeak_core, block_header, squeak):
-    with mock.patch.object(squeak_db, 'insert_squeak', autospec=True) as mock_insert_squeak, \
+    with mock.patch.object(squeak_db, 'get_number_of_squeaks', autospec=True) as mock_get_number_of_squeaks, \
+            mock.patch.object(squeak_db, 'number_of_squeaks_with_public_key_with_block_height', autospec=True) as mock_number_of_squeaks_with_public_key_with_block_height, \
+            mock.patch.object(squeak_db, 'insert_squeak', autospec=True) as mock_insert_squeak, \
             mock.patch.object(squeak_core, 'get_block_header', autospec=True) as mock_get_block_header:
+        mock_get_number_of_squeaks.return_value = 0
+        mock_number_of_squeaks_with_public_key_with_block_height.return_value = 0
         mock_get_block_header.return_value = block_header
         squeak_store.save_squeak(squeak)
 
