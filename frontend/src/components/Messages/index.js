c72ed1f7bb300b7da431484d1137c2b4c1a42656
yzernik <yzernik@gmail.com>
2022-02-16 20:49:22 -0800

    Remove content of messages page

--- frontend/src/components/Messages/index.js
+++ frontend/src/components/Messages/index.js
@@ -21,36 +21,13 @@ const Messages = (props) => {
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 888px)' })
     return(
         <React.Fragment>
-        {isTabletOrMobile && path !== '/messages' && account ? 
+        {isTabletOrMobile && path !== '/messages' && account ?
         <Chat res={true}/> :
         <div className="messages-wrapper">
             <div className="messages-header-wrapper">
                 Messages
             </div>
             <div className="messages-body">
-                <div className="recent-messages-wrapper">
-                {account && conversations && conversations.conversations.length>0 ? conversations.conversations.map(con=>{
-                    return <div style={{borderRight: path.slice(10) === con._id ? '2px solid #1da1f2' : null}} key={con._id} onClick={()=>props.history.push(`/messages/${con._id}`)} className="message-box">
-                                <Link onClick={(e)=>e.stopPropagation()} to={`/profile/${con.participants[0].username !== account.username ?
-                                         con.participants[0].username : con.participants[1].username}`} className="message-avatar">
-                                    <img width="100%" height="100" src={con.participants[0].username !== account.username ?
-                                         con.participants[0].profileImg : con.participants[1].profileImg} alt="" />
-                                </Link>
-                                {account && 
-                                <div className="message-details">
-                                    <div className="message-info">
-                                        {con.participants[0].username !== account.username ?  
-                                        <div>{con.participants[0].name}<span>@{con.participants[0].username}</span></div> :
-                                        <div>{con.participants[1].name}<span>@{con.participants[1].username}</span></div>}
-                                        <span>{moment(con.updatedAt).format("MMM D, YYYY")}</span>
-                                    </div>
-                                    <div>
-                                        {con.messages.length>0 && con.messages[con.messages.length - 1].content.slice(0,15)}
-                                    </div>
-                                </div>}
-                            </div>
-                        }): <h5 style={{ textAlign: 'center', margin: '10% auto', width:'100%' }}>You have no messages</h5>} 
-                </div>
             </div>
         </div>}
         </React.Fragment>
