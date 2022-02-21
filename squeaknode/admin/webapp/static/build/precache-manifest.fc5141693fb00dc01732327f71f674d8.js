b88d4932bbc21e649bbfbb8e5ba42be71304dead
Jonathan Zernik <yzernik@gmail.com>
2022-02-21 03:29:01 -0800

    Fix tab name for squeaks (#1953)
    * Fix tab text in profile page
    
    * Update frontend build

---
+++ squeaknode/admin/webapp/static/build/precache-manifest.fc5141693fb00dc01732327f71f674d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281380713fd92a93a50a616e76d6bd89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31847e4d92cdb3cb2f77",
+    "url": "/static/css/3.1f6bb621.chunk.css"
+  },
+  {
+    "revision": "c0dc0b3b3a0658b52e55",
+    "url": "/static/css/4.6ed76aab.chunk.css"
+  },
+  {
+    "revision": "495403b5be9ec65f08bd",
+    "url": "/static/css/5.0ea2e0af.chunk.css"
+  },
+  {
+    "revision": "b0ae1468030fcdb3157e",
+    "url": "/static/css/main.d35c01cd.chunk.css"
+  },
+  {
+    "revision": "9a5ef559636f83e448de",
+    "url": "/static/js/2.3beca443.chunk.js"
+  },
+  {
+    "revision": "a569361d31625f49e7e30aea21628ed5",
+    "url": "/static/js/2.3beca443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31847e4d92cdb3cb2f77",
+    "url": "/static/js/3.bbfd4601.chunk.js"
+  },
+  {
+    "revision": "c0dc0b3b3a0658b52e55",
+    "url": "/static/js/4.22c7ad22.chunk.js"
+  },
+  {
+    "revision": "495403b5be9ec65f08bd",
+    "url": "/static/js/5.b0e591f6.chunk.js"
+  },
+  {
+    "revision": "b0ae1468030fcdb3157e",
+    "url": "/static/js/main.f6d3ae23.chunk.js"
+  },
+  {
+    "revision": "8cac5fcd34ec750f8a7a",
+    "url": "/static/js/runtime-main.914f0a79.js"
+  },
+  {
+    "revision": "a0c2d343127a93b025409da6fd970700",
+    "url": "/static/media/icon.a0c2d343.svg"
+  }
+]);
