91ee3ddd8d1b8cae85deb3cec493c13ef40d961e
yzernik <yzernik@gmail.com>
2022-03-11 16:52:59 -0800

    Remove old client function

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -158,45 +158,6 @@ const SERVER_PORT = process.env.REACT_APP_SERVER_PORT || window.location.port;
 
 export const web_host_port = `${window.location.protocol}//${window.location.hostname}:${SERVER_PORT}`;
 
-
-export async function client(endpoint, { body, ...customConfig } = {}) {
-  const headers = { 'Content-Type': 'application/json' }
-
-  const config = {
-    method: body ? 'POST' : 'GET',
-    ...customConfig,
-    headers: {
-      ...headers,
-      ...customConfig.headers,
-    },
-  }
-
-  if (body) {
-    config.body = JSON.stringify(body)
-  }
-
-  let data
-  try {
-    const response = await window.fetch(endpoint, config)
-    data = await response.json()
-    if (response.ok) {
-      return data
-    }
-    throw new Error(response.statusText)
-  } catch (err) {
-    return Promise.reject(err.message ? err.message : data)
-  }
-}
-
-client.get = function (endpoint, customConfig = {}) {
-  return client(endpoint, { ...customConfig, method: 'GET' })
-}
-
-client.post = function (endpoint, body, customConfig = {}) {
-  return client(endpoint, { ...customConfig, body })
-}
-
-
 export const axiosBaseQuery =
   async ({ url, req, deser }) => {
     try {
