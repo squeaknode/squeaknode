30b6228f6885a3131b89fcc3b791d561964c2523
yzernik <yzernik@gmail.com>
2022-03-11 21:04:31 -0800

    Remove unused state from search page component

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -14,67 +14,6 @@ import SearchResults from '../../features/search/SearchResults'
 
 
 const Search = (props) => {
-    const { state, actions } = useContext(StoreContext);
-    const { search } = useLocation();
-    const { searchSqueaks, session } = state;
-    const [searchText, setSearchText] = useState('');
-
-    const q = useMemo(() => {
-      const p = new URLSearchParams(search);
-      const q = p.get('q');
-      return q ? decodeURIComponent(q) : '';
-    }, [search]);
-
-
-    useEffect(() => {
-        window.scrollTo(0, 0)
-        // actions.getSqueaks({lastSqueak: null})
-        if (q && q.length > 1) {
-          setSearchText(q);
-          reloadSqueaks(q);
-        }
-    }, [q])
-
-    const changeSearchText = (param) => {
-        setSearchText(param);
-    }
-
-    const searchOnEnter = (e) => {
-        if (e.keyCode === 13) {
-          if(searchText.length>0){
-            goToNewSearch(searchText);
-          }
-        }
-    }
-
-    const getLastSqueak = (squeakLst) => {
-      if (squeakLst == null) {
-        return null;
-      } if (squeakLst.length === 0) {
-        return null;
-      }
-      return squeakLst.slice(-1)[0];
-    };
-
-    const getMoreSqueaks = () => {
-        let lastSqueak = getLastSqueak(state.searchSqueaks);
-        actions.search({
-          searchText: searchText,
-          lastSqueak: lastSqueak
-        });
-    }
-
-    const reloadSqueaks = (s) => {
-        actions.clearSearch();
-        actions.search({
-          searchText: s,
-          lastSqueak: null
-        });
-    }
-
-    const goToNewSearch = (newSearchText) => {
-        props.history.push(`/app/search?q=${newSearchText}`);
-    }
 
     return (
         <div className="Home-wrapper">
