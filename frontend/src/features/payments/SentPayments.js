be053b2d7b55326e84eaf49a7e9522066e448106
yzernik <yzernik@gmail.com>
2022-03-15 19:02:42 -0700

    Make squeak hash a clickable link in payments page

--- frontend/src/features/payments/SentPayments.js
+++ frontend/src/features/payments/SentPayments.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import moment from 'moment'
 
 import SqueakCard from '../../components/SqueakCard'
@@ -45,12 +45,12 @@ const SentPayments = (props) => {
   }
 
   const renderedListItems = sentPayments.map(f=>{
-    return <div onClick={()=>goToSqueak(f.getSqueakHash())} key={f.getPaymentHash()} className="search-result-wapper">
+    return <div key={f.getPaymentHash()} className="payment-wapper">
               <div className="search-user-details">
                 <div className="search-user-warp">
                   <div className="search-user-info">
                     <div className="payment-price">{f.getPriceMsat() / 1000} sats</div>
-                    <div className="payment-squeak-hash"><b>Squeak Hash</b>: {f.getSqueakHash()}</div>
+                    <div className="payment-squeak-hash"><b>Squeak Hash</b>: <Link style={{color: "blue", fontWeight: 'bold'}} to={`/app/squeak/${f.getSqueakHash()}`}>{f.getSqueakHash()}</Link></div>
                     <div className="payment-peer-address"><b>Peer</b>: {f.getPeerAddress().getHost()}:{f.getPeerAddress().getPort()}</div>
                     <div className="payment-lightning-node"><b>Lightning Node</b>: {f.getNodePubkey()}</div>
                     <div className="payment-time">{moment(f.getTimeMs()).format("h:mm A · MMM D, YYYY")}</div>
