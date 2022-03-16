39e8b2a0b96f6a2e0d1b0c5f8f6a3b5dd1be77d6
yzernik <yzernik@gmail.com>
2022-03-15 17:26:41 -0700

    Remove connect peer button and modal from peers page

--- frontend/src/components/Peers/index.js
+++ frontend/src/components/Peers/index.js
@@ -29,7 +29,6 @@ import {
 const Peers = (props) => {
     const [tab, setTab] = useState('Connected Peers')
     const [savePeerModalOpen, setSavePeerModalOpen] = useState(false)
-    const [connectPeerModalOpen, setconnectPeerModalOpen] = useState(false)
     const [showExternalAddressModalOpen, setShowExternalAddressModalOpen] = useState(false)
     const [styleBody, setStyleBody] = useState(false)
     const [name, setName] = useState('')
@@ -73,11 +72,6 @@ const Peers = (props) => {
         setTimeout(()=>{ setSavePeerModalOpen(!savePeerModalOpen) },20)
     }
 
-    const toggleconnectPeerModal = (param, type) => {
-        setStyleBody(!styleBody)
-        setTimeout(()=>{ setconnectPeerModalOpen(!connectPeerModalOpen) },20)
-    }
-
     const toggleShowExternalAddressModalOpen = () => {
         setStyleBody(!styleBody)
         setTimeout(()=>{ setShowExternalAddressModalOpen(!showExternalAddressModalOpen) },20)
@@ -106,17 +100,6 @@ const Peers = (props) => {
         toggleSavePeerModal();
     }
 
-    const connectPeer = () => {
-        const network = getNetwork();
-        const strippedHost = removeHttp(host);
-        dispatch(setConnectPeer({
-            host: strippedHost,
-            port: port,
-            network: network,
-        }));
-        toggleconnectPeerModal();
-    }
-
     const handleChangeUseTor = () => {
       setUseTor(!useTor);
     };
@@ -142,10 +125,6 @@ const Peers = (props) => {
             <div onClick={(e)=>toggleShowExternalAddressModalOpen('edit')}
                className='profiles-create-button'>
                   <span>Show External Address</span>
-            </div>
-            <div onClick={(e)=>toggleconnectPeerModal('edit')}
-               className='profiles-create-button'>
-                  <span>Connect Peer</span>
             </div>
             <div onClick={(e)=>toggleSavePeerModal('edit')}
                className='profiles-create-button'>
@@ -279,7 +258,7 @@ const Peers = (props) => {
                     <form className="edit-form">
                     <div className="edit-input-wrap">
                         <div className="edit-input-content">
-                            <label>Name</label>
+                            <label>Name (not required)</label>
                             <input onChange={(e)=>setName(e.target.value)} type="text" name="name" className="edit-input"/>
                         </div>
                     </div>
@@ -307,55 +286,6 @@ const Peers = (props) => {
                         </label>
                         </div>
                     </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        {/* Modal for connect peer */}
-        <div onClick={()=>toggleconnectPeerModal()} style={{display: connectPeerModalOpen ? 'block' : 'none'}} className="modal-edit">
-            <div onClick={(e)=>handleModalClick(e)} className="modal-content">
-                <div className="modal-header">
-                    <div className="modal-closeIcon">
-                        <div onClick={()=>toggleconnectPeerModal()} className="modal-closeIcon-wrap">
-                            <ICON_CLOSE />
-                        </div>
-                    </div>
-                    <p className="modal-title">Connect Peer</p>
-
-                    <div className="save-modal-wrapper">
-                        <div onClick={connectPeer} className="save-modal-btn">
-                            Submit
-                        </div>
-                    </div>
-                </div>
-
-                <div className="modal-body">
-                    <form className="edit-form">
-                        <div className="edit-input-wrap">
-                            <div className="edit-input-content">
-                                <label>Host</label>
-                                <input onChange={(e)=>setHost(e.target.value)} type="text" name="name" className="edit-input"/>
-                            </div>
-                        </div>
-                        <div className="edit-input-wrap">
-                            <div className="edit-input-content">
-                                <label>Port</label>
-                                <input onChange={(e)=>setPort(e.target.value)} type="text" name="name" className="edit-input"/>
-                            </div>
-                        </div>
-                        <div className="edit-input-wrap">
-                            <div className="edit-input-content">
-                            <label>
-                            <input
-                            type="checkbox"
-                            checked={useTor}
-                            onChange={handleChangeUseTor}
-                            />
-                            Use Tor
-                            </label>
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
