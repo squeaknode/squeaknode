4af9adeeded3ed2320dd8c49d9296a170a15556d
yzernik <yzernik@gmail.com>
2022-03-15 17:37:58 -0700

    Update frontend build

--- squeaknode/admin/webapp/static/build/precache-manifest.d63830f90b931c5de8f0960d699a1571.js
+++ squeaknode/admin/webapp/static/build/precache-manifest.d63830f90b931c5de8f0960d699a1571.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cfcd5740a8ae966c08a4f9c5e228c4e1",
+    "revision": "0b255a98c80cf51b2c371c1f6f24fd8a",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.1f6bb621.chunk.css"
   },
   {
-    "revision": "df35692a3771eae60516",
+    "revision": "e412fb479d10e72daad6",
     "url": "/static/css/4.6512f3a3.chunk.css"
   },
   {
@@ -16,36 +16,36 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/5.3aaab79d.chunk.css"
   },
   {
-    "revision": "aebac4fdb8fdeeecb551",
+    "revision": "c36a7f4ae9e7eccf5a4b",
     "url": "/static/css/main.2db21378.chunk.css"
   },
   {
-    "revision": "4955e18ffa3434bb3610",
-    "url": "/static/js/2.7e46f2c7.chunk.js"
+    "revision": "552e6884ef83ffe7b186",
+    "url": "/static/js/2.babe843f.chunk.js"
   },
   {
     "revision": "a569361d31625f49e7e30aea21628ed5",
-    "url": "/static/js/2.7e46f2c7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.babe843f.chunk.js.LICENSE.txt"
   },
   {
     "revision": "8cc1987a8af1ab54b6a5",
     "url": "/static/js/3.043ad63d.chunk.js"
   },
   {
-    "revision": "df35692a3771eae60516",
-    "url": "/static/js/4.0c88bd8a.chunk.js"
+    "revision": "e412fb479d10e72daad6",
+    "url": "/static/js/4.5466ca1a.chunk.js"
   },
   {
     "revision": "98f588d1fdf51e73593e",
     "url": "/static/js/5.93db42e1.chunk.js"
   },
   {
-    "revision": "aebac4fdb8fdeeecb551",
-    "url": "/static/js/main.8343c419.chunk.js"
+    "revision": "c36a7f4ae9e7eccf5a4b",
+    "url": "/static/js/main.7c510e8d.chunk.js"
   },
   {
-    "revision": "225aa98f6e6625289fb2",
-    "url": "/static/js/runtime-main.2ae99013.js"
+    "revision": "af7b71b81945c699d35d",
+    "url": "/static/js/runtime-main.1eacdecd.js"
   },
   {
     "revision": "a0c2d343127a93b025409da6fd970700",
