61a72f4d9abb071c45a4474cfe485aef9f6e3cd0
yzernik <yzernik@gmail.com>
2022-03-15 18:30:07 -0700

    Got ctrl-click working on squeak card

--- frontend/src/components/SqueakCard/index.js
+++ frontend/src/components/SqueakCard/index.js
@@ -51,10 +51,6 @@ const SqueakCard = React.memo(function SqueakCard(props) {
         dispatch(setDeleteSqueak(id));
     }
 
-    const goToSqueak = (id) => {
-        props.history.push(`/app/squeak/${id}`)
-    }
-
     const toggleModal = (e, type) => {
         if(e){ e.stopPropagation() }
         setStyleBody(!styleBody)
@@ -99,7 +95,8 @@ const SqueakCard = React.memo(function SqueakCard(props) {
     return (
         <div>
 
-        <div onClick={()=>goToSqueak(props.id)} key={props.id} className={props.squeak ? "Squeak-card-wrapper" : "Squeak-card-wrapper missing-squeak"} >
+        <Link onClick={(e)=>e.stopPropagation()} to={`/app/squeak/${props.id}`}>
+        <div key={props.id} className={props.squeak ? "Squeak-card-wrapper" : "Squeak-card-wrapper missing-squeak"} >
 
             {props.squeak ?
               <>
@@ -209,6 +206,10 @@ const SqueakCard = React.memo(function SqueakCard(props) {
                 </div>
             </div> : null}
         </div> : null}
+
+        </Link>
+
+
         </div>
     )
 });
