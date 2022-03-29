6f356a6800a4016d29b2fa8de52623429a89fb95
yzernik <yzernik@gmail.com>
2022-03-28 23:06:29 -0700

    Got add peer form on peers page working with tesla informed library

--- frontend/src/components/Peers/index.js
+++ frontend/src/components/Peers/index.js
@@ -7,6 +7,8 @@ import Loader from '../Loader'
 import SqueakCard from '../SqueakCard'
 import PeerCard from '../../features/peers/PeerCard'
 
+import { Form, Input, Select, Checkbox, Relevant, Debug, TextArea, Option } from 'informed';
+
 import { unwrapResult } from '@reduxjs/toolkit'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
@@ -66,7 +68,7 @@ const Peers = (props) => {
         setTimeout(()=>{ setShowExternalAddressModalOpen(!showExternalAddressModalOpen) },20)
     }
 
-    const getNetwork = () => {
+    const getNetwork = (useTor) => {
       if (useTor) {
         return 'TORV3';
       }
@@ -77,13 +79,13 @@ const Peers = (props) => {
       return url.replace(/^https?:\/\//, '');
     }
 
-    const savePeer = () => {
-        const network = getNetwork();
-        const strippedHost = removeHttp(host);
+    const savePeer = ({values}) => {
+        const network = getNetwork(values.useTor);
+        const strippedHost = removeHttp(values.host);
         dispatch(setSavePeer({
-            name: name,
+            name: values.name,
             host: strippedHost,
-            port: port,
+            port: values.port,
             network: network,
         }));
         toggleSavePeerModal();
@@ -96,6 +98,35 @@ const Peers = (props) => {
     const handleModalClick = (e) => {
         e.stopPropagation()
     }
+
+    const AddPeerForm = () => (
+      <Form onSubmit={savePeer} className="Squeak-input-side">
+        <div className="edit-input-wrap">
+          <Input class="informed-input" name="name" label="Peer Name (not required)" />
+        </div>
+        <div className="edit-input-wrap">
+          <Input class="informed-input" name="host" label="Host" />
+        </div>
+        <div className="edit-input-wrap">
+          <Input class="informed-input" name="port" label="Port" />
+        </div>
+        <div className="edit-input-wrap">
+          <Checkbox class="informed-input" name="useTor" label="Connect With Tor" />
+        </div>
+
+        <div className="inner-input-links">
+          <div className="input-links-side">
+          </div>
+          <div className="squeak-btn-holder">
+            <div style={{ fontSize: '13px', color: null }}>
+            </div>
+            <button type="submit" className={'squeak-btn-side squeak-btn-active'}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </Form>
+    );
 
 
     return(
@@ -186,48 +217,12 @@ const Peers = (props) => {
                             <ICON_CLOSE />
                         </div>
                     </div>
-                    <p className="modal-title">Save Peer</p>
+                    <p className="modal-title">Add Peer</p>
 
-                    <div className="save-modal-wrapper">
-                        <div onClick={savePeer} className="save-modal-btn">
-                            Submit
-                        </div>
-                    </div>
                 </div>
 
                 <div className="modal-body">
-                    <form className="edit-form">
-                    <div className="edit-input-wrap">
-                        <div className="edit-input-content">
-                            <label>Name (not required)</label>
-                            <input onChange={(e)=>setName(e.target.value)} type="text" name="name" className="edit-input"/>
-                        </div>
-                    </div>
-                    <div className="edit-input-wrap">
-                        <div className="edit-input-content">
-                            <label>Host</label>
-                            <input onChange={(e)=>setHost(e.target.value)} type="text" name="name" className="edit-input"/>
-                        </div>
-                    </div>
-                    <div className="edit-input-wrap">
-                        <div className="edit-input-content">
-                            <label>Port</label>
-                            <input onChange={(e)=>setPort(e.target.value)} type="text" name="name" className="edit-input"/>
-                        </div>
-                    </div>
-                    <div className="edit-input-wrap">
-                        <div className="edit-input-content">
-                        <label>
-                        <input
-                        type="checkbox"
-                        checked={useTor}
-                        onChange={handleChangeUseTor}
-                        />
-                        Use Tor
-                        </label>
-                        </div>
-                    </div>
-                    </form>
+                    <AddPeerForm />
                 </div>
             </div>
         </div>
