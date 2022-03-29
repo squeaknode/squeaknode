1a351230540e42adf5712ed181a875a9906443c2
yzernik <yzernik@gmail.com>
2022-03-28 18:54:30 -0700

    Improved make squeak form using tesla informed library

--- frontend/src/features/squeaks/MakeSqueak.js
+++ frontend/src/features/squeaks/MakeSqueak.js
@@ -7,7 +7,8 @@ import ContentEditable from 'react-contenteditable'
 import { Link } from 'react-router-dom'
 import { getProfileImageSrcString } from '../../squeakimages/images';
 import Loader from '../../components/Loader'
-import Select from 'react-select'
+
+import { Form, Input, Select, Checkbox, Relevant, Debug, TextArea, Option } from 'informed';
 
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
@@ -23,152 +24,136 @@ import {
 } from '../../features/profiles/profilesSlice'
 
 const MakeSqueak = (props) => {
-    const signingProfiles = useSelector(selectSigningProfiles);
-    const makeSqueakStatus = useSelector(selectMakeSqueakStatus);
-    const dispatch = useDispatch();
+  const signingProfiles = useSelector(selectSigningProfiles);
+  const makeSqueakStatus = useSelector(selectMakeSqueakStatus);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchSigningProfiles());
-    }, [])
+  useEffect(() => {
+    dispatch(fetchSigningProfiles());
+  }, [])
 
-    //used for contenteditable divs on react hooks
-    const squeakT = useRef('');
-    const handleChange = (evt, e) => {
-        if (squeakT.current.trim().length <= 280
-            && squeakT.current.split(/\r\n|\r|\n/).length <= 30) {
-            squeakT.current = evt.target.value;
-            setSqueakText(squeakT.current)
-        }
-        // document.getElementById('squeak-box').innerHTML = document.getElementById('squeak-box').innerHTML.replace(/(\#\w+)/g, '<span class="blue">$1</span>')
-    };
-    const [signingProfile, setSigningProfile] = useState(null)
-    const [squeakText, setSqueakText] = useState("")
 
-    const optionsFromProfiles = (profiles) => {
-      return profiles.map((p) => {
-          return { value: p, label: p.getProfileName() }
-          // return { value: 'chocolate', label: 'Chocolate' }
-        });
+  const submitSqueak = ({ values }) => {
+    // TODO: toggle modal off here.
+    console.log(values);
+    console.log(values.signingProfileId);
+    console.log(values.content);
+
+    if (!values.signingProfileId) {
+      alert('signingProfileId must be set.');
+      return;
     }
 
-    const handleChangeSigningProfile = (e) => {
-      setSigningProfile(e.value);
+    if (!values.content.length) { return }
+
+    const makeValues = {
+      signingProfile: values.signingProfileId,
+      description: values.content,
+      replyTo: props.replyToSqueak ? props.replyToSqueak.getSqueakHash() : null,
+      hasRecipient: null,
+      recipientProfileId: -1,
     }
-
-    const pastePlainText = (e) => {
-      e.preventDefault();
-      var text = e.clipboardData.getData('text/plain');
-      document.execCommand('insertText', false, text);
+    console.log('makeSqueak');
+    dispatch(setMakeSqueak(makeValues))
+    .then(unwrapResult)
+    .then((squeakHash) => {
+      props.history.push(`/app/squeak/${squeakHash}`);
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+    if (props.submittedCallback) {
+      props.submittedCallback();
     }
+  }
 
-    const submitSqueak = () => {
-        // TODO: toggle modal off here.
+  // TODO: Show profile image for selected signing profile.
+  // <div className="Squeak-profile-wrapper">
+  //   {signingProfile && <img alt="" style={{ borderRadius: '50%', minWidth: '49px' }} width="100%" height="49px" src={`${getProfileImageSrcString(signingProfile)}`} />}
+  // </div>
 
-        if (!signingProfile) {
-          alert('Signing profile must be set.');
-          return;
-        }
+  const author = props.replyToSqueak && props.replyToSqueak.getAuthor();
 
-        if (!squeakText.length) { return }
+  const MakeSqueakForm = () => (
+    <Form onSubmit={submitSqueak} className="Squeak-input-side">
+        <div className="inner-input-box">
+          <Select name="signingProfileId" initialValue="">
+            <Option value="" disabled>
+              Select Signing Profile...
+            </Option>
+            {signingProfiles.map(p => {
+              return <option value={p.getProfileId()}>{p.getProfileName()}</option>
+            })}
+          </Select>
+        </div>
+        <div className="inner-input-box">
+          <TextArea name="content" placeholder="What's happening..." />
+        </div>
 
-        const values = {
-            signingProfile: signingProfile.getProfileId(),
-            description: squeakText,
-            replyTo: props.replyToSqueak ? props.replyToSqueak.getSqueakHash() : null,
-            hasRecipient: null,
-            recipientProfileId: -1,
-        }
-        console.log('makeSqueak');
-        dispatch(setMakeSqueak(values))
-          .then(unwrapResult)
-          .then((squeakHash) => {
-            props.history.push(`/app/squeak/${squeakHash}`);
-          })
-          .catch((err) => {
-            alert(err.message);
-          });
-        squeakT.current = ''
-        setSqueakText('')
-        if (props.submittedCallback) {
-          props.submittedCallback();
-        }
-    }
+        <div className="inner-input-links">
+            <div className="input-links-side">
+            </div>
+            <div className="squeak-btn-holder">
+                <div style={{ fontSize: '13px', color: null }}>
 
-    const author = props.replyToSqueak && props.replyToSqueak.getAuthor();
+                </div>
+                <button type="submit" className={'squeak-btn-side squeak-btn-active'}>
+                  Squeak
+                </button>
+            </div>
+        </div>
+    </Form>
+  );
 
 
-    return (
-      <>
+  return (
+    <>
 
-      {/* Squeak being replied to. */}
-      {props.replyToSqueak ?
+    {/* Squeak being replied to. */}
+    {props.replyToSqueak ?
       <div className="reply-content-wrapper">
-          <div className="card-userPic-wrapper">
-              <Link onClick={(e)=>e.stopPropagation()} to={`/app/profile/${props.replyToSqueak.getAuthorPubkey()}`}>
-                  <img alt="" style={{borderRadius:'50%', minWidth:'49px'}} width="100%" height="49px" src={author ? `${getProfileImageSrcString(props.replyToSqueak.getAuthor())}`: null}/>
-              </Link>
+        <div className="card-userPic-wrapper">
+          <Link onClick={(e)=>e.stopPropagation()} to={`/app/profile/${props.replyToSqueak.getAuthorPubkey()}`}>
+            <img alt="" style={{borderRadius:'50%', minWidth:'49px'}} width="100%" height="49px" src={author ? `${getProfileImageSrcString(props.replyToSqueak.getAuthor())}`: null}/>
+          </Link>
+        </div>
+        <div className="card-content-wrapper">
+          <div className="card-content-header">
+            <div className="card-header-detail">
+              <span className="card-header-user">
+                <Link onClick={(e)=>e.stopPropagation()} to={`/app/profile/${props.replyToSqueak.getAuthorPubkey()}`}>{author ? author.getProfileName(): 'Unknown Author'}</Link>
+              </span>
+              <span className="card-header-username">
+                <Link onClick={(e)=>e.stopPropagation()} to={`/app/profile/${props.replyToSqueak.getAuthorPubkey()}`}>{'@'+props.replyToSqueak.getAuthorPubkey()}</Link>
+              </span>
+              <span className="card-header-dot">·</span>
+              <span className="card-header-date">
+                {moment(props.replyToSqueak.getBlockTime() * 1000).fromNow()}
+              </span>
+            </div>
           </div>
-          <div className="card-content-wrapper">
-              <div className="card-content-header">
-                  <div className="card-header-detail">
-                      <span className="card-header-user">
-                          <Link onClick={(e)=>e.stopPropagation()} to={`/app/profile/${props.replyToSqueak.getAuthorPubkey()}`}>{author ? author.getProfileName(): 'Unknown Author'}</Link>
-                      </span>
-                      <span className="card-header-username">
-                          <Link onClick={(e)=>e.stopPropagation()} to={`/app/profile/${props.replyToSqueak.getAuthorPubkey()}`}>{'@'+props.replyToSqueak.getAuthorPubkey()}</Link>
-                      </span>
-                      <span className="card-header-dot">·</span>
-                      <span className="card-header-date">
-                                  {moment(props.replyToSqueak.getBlockTime() * 1000).fromNow()}
-                      </span>
-                  </div>
-              </div>
-              <div className="card-content-info">
-              {props.replyToSqueak.getContentStr()}
-              </div>
-              <div className="reply-to-user">
-                  <span className="reply-squeak-username">
-                      Replying to
-                  </span>
-                  <span className="main-squeak-user">
-                      @{props.replyToSqueak.getAuthorPubkey()}
-                  </span>
-              </div>
+          <div className="card-content-info">
+            {props.replyToSqueak.getContentStr()}
           </div>
+          <div className="reply-to-user">
+            <span className="reply-squeak-username">
+              Replying to
+            </span>
+            <span className="main-squeak-user">
+              @{props.replyToSqueak.getAuthorPubkey()}
+            </span>
+          </div>
+        </div>
       </div> : null }
 
 
-            {/* New squeak content input. */}
-            <div className="Squeak-input-wrapper">
-                <div className="Squeak-profile-wrapper">
-                    <Link to={`/app/profile/${signingProfile && signingProfile.getPubkey()}`}>
-                        {signingProfile && <img alt="" style={{ borderRadius: '50%', minWidth: '49px' }} width="100%" height="49px" src={`${getProfileImageSrcString(signingProfile)}`} />}
-                    </Link>
-                </div>
-                <div className="Squeak-input-side">
-                    <div className="inner-input-box">
-                        <Select options={optionsFromProfiles(signingProfiles)} onChange={handleChangeSigningProfile} />
-                    </div>
-                    <div className="inner-input-box">
-                        <ContentEditable onPaste={(e) => pastePlainText(e)} id="squeak-box" className={squeakText.length ? 'squeak-input-active' : null} onKeyDown={(e)=>squeakT.current.length>279 ? e.keyCode !== 8 && e.preventDefault(): null} placeholder="What's happening?" html={squeakT.current} onChange={(e) => handleChange(e)} />
-                    </div>
-                    <div className="inner-input-links">
-                        <div className="input-links-side">
-                        </div>
-                        <div className="squeak-btn-holder">
-                            <div style={{ fontSize: '13px', color: squeakText.length >= 280 ? 'red' : null }}>
-                                {squeakText.length > 0 && squeakText.length + '/280'}
-                            </div>
-                            <div onClick={submitSqueak} className={squeakText.length ? 'squeak-btn-side squeak-btn-active' : 'squeak-btn-side'}>
-                                Squeak
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      {/* New squeak content input. */}
+      <div className="Squeak-input-wrapper">
+        <MakeSqueakForm />
+      </div>
 
-            </>
-    )
+      </>
+  )
 }
 
 export default withRouter(MakeSqueak)
