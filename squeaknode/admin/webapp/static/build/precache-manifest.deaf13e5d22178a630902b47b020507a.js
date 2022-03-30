dccc9ac185a09a48a1e2a888d9a93a2b05f643b7
yzernik <yzernik@gmail.com>
2022-03-30 01:51:48 -0700

    Update frontend build

---
+++ squeaknode/admin/webapp/static/build/precache-manifest.deaf13e5d22178a630902b47b020507a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ff99f611aae6aa430cedbdcc246108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54c1e9d2b8060e37a08a",
+    "url": "/static/css/3.1f6bb621.chunk.css"
+  },
+  {
+    "revision": "e706ac77a63cb706fbf7",
+    "url": "/static/css/4.6512f3a3.chunk.css"
+  },
+  {
+    "revision": "906887393fd03dc9f3bd",
+    "url": "/static/css/5.3aaab79d.chunk.css"
+  },
+  {
+    "revision": "b16c9afd5ad8c66a26af",
+    "url": "/static/css/main.be11d9c8.chunk.css"
+  },
+  {
+    "revision": "2f5c8fbe8561a635891f",
+    "url": "/static/js/2.02f1cb70.chunk.js"
+  },
+  {
+    "revision": "a569361d31625f49e7e30aea21628ed5",
+    "url": "/static/js/2.02f1cb70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c1e9d2b8060e37a08a",
+    "url": "/static/js/3.f8daaa08.chunk.js"
+  },
+  {
+    "revision": "e706ac77a63cb706fbf7",
+    "url": "/static/js/4.5d28b4fe.chunk.js"
+  },
+  {
+    "revision": "906887393fd03dc9f3bd",
+    "url": "/static/js/5.01c68ef6.chunk.js"
+  },
+  {
+    "revision": "b16c9afd5ad8c66a26af",
+    "url": "/static/js/main.c7ca5a55.chunk.js"
+  },
+  {
+    "revision": "40ad6039c0c397469cff",
+    "url": "/static/js/runtime-main.f97cd83d.js"
+  },
+  {
+    "revision": "a0c2d343127a93b025409da6fd970700",
+    "url": "/static/media/icon.a0c2d343.svg"
+  }
+]);
