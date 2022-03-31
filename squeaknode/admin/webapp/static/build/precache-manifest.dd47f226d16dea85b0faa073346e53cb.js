d68e888b8d24c26b0cb2cf5c894be4facf339db4
yzernik <yzernik@gmail.com>
2022-03-30 21:17:42 -0700

    Update frontend build

---
+++ squeaknode/admin/webapp/static/build/precache-manifest.dd47f226d16dea85b0faa073346e53cb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bdf187ee95230950f194530b73c2c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b231ad02f3f1cc4d806e",
+    "url": "/static/css/3.1f6bb621.chunk.css"
+  },
+  {
+    "revision": "73f49a16160afba90031",
+    "url": "/static/css/4.6512f3a3.chunk.css"
+  },
+  {
+    "revision": "8671e1a9be8d91740510",
+    "url": "/static/css/5.3aaab79d.chunk.css"
+  },
+  {
+    "revision": "8f5e1c1f2d6d6f4df7e1",
+    "url": "/static/css/main.be11d9c8.chunk.css"
+  },
+  {
+    "revision": "d847f8d7fccd469909c7",
+    "url": "/static/js/2.34efa20b.chunk.js"
+  },
+  {
+    "revision": "a569361d31625f49e7e30aea21628ed5",
+    "url": "/static/js/2.34efa20b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b231ad02f3f1cc4d806e",
+    "url": "/static/js/3.2d2d34d3.chunk.js"
+  },
+  {
+    "revision": "73f49a16160afba90031",
+    "url": "/static/js/4.13c6f568.chunk.js"
+  },
+  {
+    "revision": "8671e1a9be8d91740510",
+    "url": "/static/js/5.2d635141.chunk.js"
+  },
+  {
+    "revision": "8f5e1c1f2d6d6f4df7e1",
+    "url": "/static/js/main.6a7c927e.chunk.js"
+  },
+  {
+    "revision": "a02b2d7c374aeb856b49",
+    "url": "/static/js/runtime-main.05610a5d.js"
+  },
+  {
+    "revision": "a0c2d343127a93b025409da6fd970700",
+    "url": "/static/media/icon.a0c2d343.svg"
+  }
+]);
--- squeaknode/admin/webapp/static/build/service-worker.js
+++ squeaknode/admin/webapp/static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd28673d075bf2731e351fd5dcf09cd2.js"
+  "/precache-manifest.dd47f226d16dea85b0faa073346e53cb.js"
 );
 
 self.addEventListener('message', (event) => {
